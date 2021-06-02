60b759953443cf027bc867a2807c6c1bbc5d95fe
MUHAMMAD <85131843+JMTHON-AR@users.noreply.github.com>
2021-06-02 16:22:16 +0300

    Add files via upload

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -1,92 +1,78 @@
-import re
 import time
 from platform import python_version
 
 from telethon import version
-from telethon.events import CallbackQuery
 
-from userbot import StartTime, catub, catversion
+from . import ALIVE_NAME, StartTime, catversion, get_readable_time, mention, reply_id
 
-from ..Config import Config
-from ..core.managers import edit_or_reply
-from ..helpers.functions import catalive, check_data_base_heal_th, get_readable_time
-from ..helpers.utils import reply_id
-from . import mention
-
+DEFAULTUSER = ALIVE_NAME or "𝗝𝗠𝗧𝗛𝗢𝗡♡⁩"
 CAT_IMG = Config.ALIVE_PIC or "https://telegra.ph/file/8c85ff635e3736629c3c2.jpg"
-CUSTOM_ALIVE_TEXT = Config.CUSTOM_ALIVE_TEXT or "𝗪َ𝗘𝗟َِ𝗖𝗢𝗠َِ𝙀َِ 𝗧𝗢 𝗝𝗠𝗧𝗛𝗢𝗡 𝄵 ⇜"
-EMOJI = Config.CUSTOM_ALIVE_EMOJI or "-""
-
-plugin_category = "utils"
+CUSTOM_ALIVE_TEXT = Config.CUSTOM_ALIVE_TEXT or "⇝ 𝗪َ𝗘𝗟َِ𝗖𝗢𝗠َِ𝙀َِ 𝗧𝗢 𝗝𝗠𝗧𝗛𝗢𝗡 𝄵 ⇜"
+EMOJI = Config.CUSTOM_ALIVE_EMOJI or "-"
 
 
-@catub.cat_cmd(
-    pattern="فحص$",
-    command=("فحص", plugin_category),
-    info={
-        "header": "To check bot's alive status",
-        "options": "To show media in this cmd you need to set ALIVE_PIC with media link, get this by replying the media by .tgm",
-        "usage": [
-            "{tr}alive",
-        ],
-    },
-)
-async def amireallyalive(event):
-    "A kind of showing bot details"
-    reply_to_id = await reply_id(event)
+@bot.on(admin_cmd(outgoing=True, pattern="فحص$"))
+@bot.on(sudo_cmd(pattern="فحص$", allow_sudo=True))
+async def amireallyalive(alive):
+    if alive.fwd_from:
+        return
+    reply_to_id = await reply_id(alive)
     uptime = await get_readable_time((time.time() - StartTime))
     _, check_sgnirts = check_data_base_heal_th()
-    if Config.ALIVE_PIC:
+    if CAT_IMG:
         cat_caption = f"**{CUSTOM_ALIVE_TEXT}**\n\n"
         cat_caption += f"**{EMOJI} قاعدۿ البيانات :** `{check_sgnirts}`\n"
-        cat_caption += f"**{EMOJI} نسخۿ التيليثون :** `{version.__version__}\n`"
+        cat_caption += f"**{EMOJI} نسخۿ تليثون :** `{version.__version__}\n`"
         cat_caption += f"**{EMOJI} نسخـۿ جـمثون:** `{catversion}`\n"
         cat_caption += f"**{EMOJI} نسخـۿ البايثون :** `{python_version()}\n`"
         cat_caption += f"**{EMOJI} الوقت :** `{uptime}\n`"
         cat_caption += f"**{EMOJI} المنشئ:** {mention}\n"
         cat_caption += f"**{EMOJI}**  **[𝗦𝗼𝘂𝗿𝗰𝗲 𝗖𝗵𝗮𝗻𝗻𝗲𝗹]**(t.me/jmthon)   .\n"
-        await event.client.send_file(
-            event.chat_id, Config.ALIVE_PIC, caption=cat_caption, reply_to=reply_to_id
+        await alive.client.send_file(
+            alive.chat_id, CAT_IMG, caption=cat_caption, reply_to=reply_to_id
         )
-        await event.delete()
+        await alive.delete()
     else:
         await edit_or_reply(
-            event,
-            f"**{CUSTOM_ALIVE_TEXT}**\n\n"
-            f"**{EMOJI} قـاعدة البيانات :** `{check_sgnirts}`\n"
-            f"**{EMOJI} اصـدار التيليثون :** `{version.__version__}\n`"
-            f"**{EMOJI} اصـدار جـمثون :** `{catversion}`\n"
-            f"**{EMOJI} اصـدار البـايثون :** `{python_version()}\n`"
-            f"**{EMOJI} الوقت :** `{uptime}\n`"
-            f"**{EMOJI} المنشئ:** {mention}\n",
+            alive,
+             f"**{CUSTOM_ALIVE_TEXT}**\n"
+        f"**{CUSTOM_ALIVE_TEXT}**\n"
+        f"**{EMOJI} قاعدة البيانات 『** `1.0.0`』\n"
+        f"**{EMOJI} اصدار الـتليثون  『** `1.0.0`』\n`"
+        f"**{EMOJI} اصدار جـمثون  『 `1.0.0`』**\n`"
+        f"**{EMOJI} اصدار البـايثون  『 `1.0.0`』**\n`"
+        f"**{EMOJI} مدة التشغيل 『** `{uptime}』\n`"
+        f"**{EMOJI} المستخدم 『** {mention}』\n"
         )
 
 
-@catub.cat_cmd(
-    pattern="السورس$",
-    command=("السورس", plugin_category),
-    info={
-        "header": "To check bot's alive status via inline mode",
-        "options": "To show media in this cmd you need to set ALIVE_PIC with media link, get this by replying the media by .tgm",
-        "usage": [
-            "{tr}alive",
-        ],
-    },
+def check_data_base_heal_th():
+    # https://stackoverflow.com/a/41961968
+    is_database_working = False
+    output = "لم يتم تعيين قاعدة بيانات"
+    if not Config.DB_URI:
+        return is_database_working, output
+    from userbot.plugins.sql_helper import SESSION
+
+    try:
+        # to check database we will execute raw query
+        SESSION.execute("SELECT 1")
+    except Exception as e:
+        output = f"❌ {str(e)}"
+        is_database_working = False
+    else:
+        output = "تعمل بنجاح"
+        is_database_working = True
+    return is_database_working, output
+
+
+CMD_HELP.update(
+    {
+        "alive": "**Plugin :** `sourc`\
+      \n\n  •  **Syntax : **`.sourc` \
+      \n  •  **Function : **__status of bot will be showed__\
+      \n\n  •  **Syntax : **`.` \
+      \n  •  **Function : **__inline status of bot will be shown.__\
+      \nSet `ALIVE_PIC` var for media in alive message"
+    }
 )
-async def amireallyalive(event):
-    "A kind of showing bot details by your inline bot"
-    reply_to_id = await reply_id(event)
-    cat_caption = f"**{CUSTOM_ALIVE_TEXT}**\n"
-    cat_caption += f"**{EMOJI} اصـدار التيليثون :** `{version.__version__}\n`"
-    cat_caption += f"**{EMOJI} اصـدار جـمثون :** `{catversion}`\n"
-    cat_caption += f"**{EMOJI} اصـدار البـايثون :** `{python_version()}\n`"
-    cat_caption += f"**{EMOJI} المنشئ:** {mention}\n"
-    results = await event.client.inline_query(Config.TG_BOT_USERNAME, cat_caption)
-    await results[0].click(event.chat_id, reply_to=reply_to_id, hide_via=True)
-    await event.delete()
-
-
-@catub.tgbot.on(CallbackQuery(data=re.compile(b"stats")))
-async def on_plug_in_callback_query_handler(event):
-    statstext = await catalive(StartTime)
-    await event.answer(statstext, cache_time=0, alert=True)
