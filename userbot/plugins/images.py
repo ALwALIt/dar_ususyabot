c89b3a0c317f81388a5afbf45c0c4e8ea18f109b
MUHAMMAD <85131843+JMTHON-AR@users.noreply.github.com>
2021-06-05 04:44:01 +0300

    Update images.py

--- userbot/plugins/images.py
+++ userbot/plugins/images.py
@@ -2,31 +2,14 @@
 import os
 import shutil
 
-from userbot import catub
-
-from ..core.managers import edit_or_reply
 from ..helpers.google_image_download import googleimagesdownload
-from ..helpers.utils import reply_id
-
-plugin_category = "misc"
 
 
-@catub.cat_cmd(
-    pattern="img(?: |$)(\d*)? ?(.*)",
-    command=("img", plugin_category),
-    info={
-        "header": "Google image search.",
-        "description": "To search images in google. By default will send 3 images.you can get more images(upto 10 only by changing limit value as shown in usage and examples.",
-        "usage": ["{tr}img <1-10> <query>", "{tr}img <query>"],
-        "examples": [
-            "{tr}img 10 catuserbot",
-            "{tr}img catuserbot",
-            "{tr}img 7 catuserbot",
-        ],
-    },
-)
+@bot.on(admin_cmd(pattern=r"بحث صور(?: |$)(\d*)? ?(.*)"))
+@bot.on(sudo_cmd(pattern=r"بحث صور(?: |$)(\d*)? ?(.*)", allow_sudo=True))
 async def img_sampler(event):
-    "Google image search."
+    if event.fwd_from:
+        return
     reply_to_id = await reply_id(event)
     if event.is_reply and not event.pattern_match.group(2):
         query = await event.get_reply_message()
@@ -34,10 +17,8 @@ async def img_sampler(event):
     else:
         query = str(event.pattern_match.group(2))
     if not query:
-        return await edit_or_reply(
-            event, "Reply to a message or pass a query to search!"
-        )
-    cat = await edit_or_reply(event, "`Processing...`")
+        return await edit_or_reply(event, "الرد على رسالة للبحث او ضعها مع الامر")
+    cat = await edit_or_reply(event, "جاري بحث عن صور")
     if event.pattern_match.group(1) != "":
         lim = int(event.pattern_match.group(1))
         if lim > 10:
@@ -63,3 +44,12 @@ async def img_sampler(event):
     await event.client.send_file(event.chat_id, lst, reply_to=reply_to_id)
     shutil.rmtree(os.path.dirname(os.path.abspath(lst[0])))
     await cat.delete()
+
+
+CMD_HELP.update(
+    {
+        "images": "**Plugin :**`images`\
+    \n\n**  •  Syntax :** `.img <limit> <Name>` or `.img <limit> (replied message)`\
+    \n**  •  Function : **do google image search and sends 3 images. default if you havent mentioned limit"
+    }
+)
