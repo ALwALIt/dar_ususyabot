0236f3206a137cab7519b7643e382b58458d52e1
JMTHON <85131843+JMTHON-AR@users.noreply.github.com>
2021-06-05 15:57:18 +0300

    Add files via upload

--- userbot/plugins/clone.py
+++ userbot/plugins/clone.py
@@ -21,20 +21,20 @@ DEFAULTUSER = str(AUTONAME) if AUTONAME else str(ALIVE_NAME)
 DEFAULTUSERBIO = (
     str(DEFAULT_BIO)
     if DEFAULT_BIO
-    else "sıɥʇ ǝpoɔǝp uǝɥʇ llıʇu∩ ˙ ǝɔɐds ǝʇɐʌıɹd ǝɯos ǝɯ ǝʌı⅁˙"
+    else "يفشل الأمر حين يتحول إلى واجب بدلًا من رغبة  - @JMTHON"
 )
 
 
 @catub.cat_cmd(
-    pattern="clone(?: |$)(.*)",
-    command=("clone", plugin_category),
+    pattern="انتحال(?: |$)(.*)",
+    command=("انتحال", plugin_category),
     info={
         "header": "To clone account of mentiond user or replied user",
         "usage": "{tr}clone <username/userid/reply>",
     },
 )
 async def _(event):
-    "To clone account of mentiond user or replied user"
+    "يجب الرد على المستخدم لنسخ حسابه بالكامل"
     replied_user, error_i_a = await get_user_from_event(event)
     if replied_user is None:
         return
@@ -58,17 +58,17 @@ async def _(event):
     await event.client(functions.account.UpdateProfileRequest(about=user_bio))
     pfile = await event.client.upload_file(profile_pic)
     await event.client(functions.photos.UploadProfilePhotoRequest(pfile))
-    await edit_delete(event, "**LET US BE AS ONE**")
+    await edit_delete(event, "**تم نسخ الحساب بنجاح ✅**")
     if BOTLOG:
         await event.client.send_message(
             BOTLOG_CHATID,
-            f"#CLONED\nSuccesfully cloned [{first_name}](tg://user?id={user_id })",
+            f"#الانتحال\nتم بنجاح الانتحال [{first_name}](tg://user?id={user_id })",
         )
 
 
 @catub.cat_cmd(
-    pattern="revert$",
-    command=("revert", plugin_category),
+    pattern="اعادة$",
+    command=("اعادة", plugin_category),
     info={
         "header": "To revert back to your original name , bio and profile pic",
         "note": "For proper Functioning of this command you need to set AUTONAME and DEFAULT_BIO with your profile name and bio respectively.",
@@ -88,8 +88,8 @@ async def _(event):
     await event.client(functions.account.UpdateProfileRequest(about=bio))
     await event.client(functions.account.UpdateProfileRequest(first_name=name))
     await event.client(functions.account.UpdateProfileRequest(last_name=blank))
-    await edit_delete(evenr, "succesfully reverted to your account back")
+    await edit_delete(evenr, "**تك اعادة الحساب بنجاح ✅**")
     if BOTLOG:
         await event.client.send_message(
-            BOTLOG_CHATID, f"#REVERT\nSuccesfully reverted back to your profile"
+            BOTLOG_CHATID, f"#الاعادة\nتم ارجاع الحساب الى وضعه الطبيعي بنجاح ✅"
         )
