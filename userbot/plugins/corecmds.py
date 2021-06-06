02a3d281afc5533d6169545ddc6f1b39e3e2f6bf
JMTHON <85131843+JMTHON-AR@users.noreply.github.com>
2021-06-06 20:23:41 +0300

    Add files via upload

--- userbot/plugins/corecmds.py
+++ userbot/plugins/corecmds.py
@@ -12,8 +12,8 @@ thumb_image_path = os.path.join(Config.TMP_DOWNLOAD_DIRECTORY, "thumb_image.jpg"
 
 
 @catub.cat_cmd(
-    pattern="install$",
-    command=("install", plugin_category),
+    pattern="تنصيب$",
+    command=("تنصيب", plugin_category),
     info={
         "header": "To install an external plugin.",
         "description": "Reply to any external plugin(supported by cat) to install it in your bot.",
@@ -34,101 +34,22 @@ async def install(event):
                 load_module(shortname.replace(".py", ""))
                 await edit_delete(
                     event,
-                    f"Installed Plugin `{os.path.basename(downloaded_file_name)}`",
+                    f"تـم تثبيـت المـلف `{os.path.basename(downloaded_file_name)}`",
                     10,
                 )
             else:
                 os.remove(downloaded_file_name)
                 await edit_delete(
-                    event, "Errors! This plugin is already installed/pre-installed.", 10
+                    event, "**خـطأ  هذا الملف تم تنصيبه مسبقا او يوجد في السورس **", 10
                 )
         except Exception as e:
-            await edit_delete(event, f"**Error:**\n`{str(e)}`", 10)
+            await edit_delete(event, f"**خـطأ:**\n`{str(e)}`", 10)
             os.remove(downloaded_file_name)
 
 
 @catub.cat_cmd(
-    pattern="load (.*)",
-    command=("load", plugin_category),
-    info={
-        "header": "To load a plugin again. if you have unloaded it",
-        "description": "To load a plugin again which you unloaded by {tr}unload",
-        "usage": "{tr}load <plugin name>",
-        "examples": "{tr}load markdown",
-    },
-)
-async def load(event):
-    "To load a plugin again. if you have unloaded it"
-    shortname = event.pattern_match.group(1)
-    try:
-        try:
-            remove_plugin(shortname)
-        except BaseException:
-            pass
-        load_module(shortname)
-        await edit_delete(event, f"`Successfully loaded {shortname}`", 10)
-    except Exception as e:
-        await edit_or_reply(
-            event,
-            f"Could not load {shortname} because of the following error.\n{str(e)}",
-        )
-
-
-@catub.cat_cmd(
-    pattern="send (.*)",
-    command=("send", plugin_category),
-    info={
-        "header": "To upload a plugin file to telegram chat",
-        "usage": "{tr}send <plugin name>",
-        "examples": "{tr}send markdown",
-    },
-)
-async def send(event):
-    "To uplaod a plugin file to telegram chat"
-    reply_to_id = await reply_id(event)
-    thumb = None
-    if os.path.exists(thumb_image_path):
-        thumb = thumb_image_path
-    input_str = event.pattern_match.group(1)
-    the_plugin_file = f"./userbot/plugins/{input_str}.py"
-    if os.path.exists(the_plugin_file):
-        caat = await event.client.send_file(
-            event.chat_id,
-            the_plugin_file,
-            force_document=True,
-            allow_cache=False,
-            reply_to=reply_to_id,
-            thumb=thumb,
-            caption=f"**➥ Plugin Name:-** `{input_str}`",
-        )
-        await event.delete()
-    else:
-        await edit_or_reply(event, "404: File Not Found")
-
-
-@catub.cat_cmd(
-    pattern="unload (.*)",
-    command=("unload", plugin_category),
-    info={
-        "header": "To unload a plugin temporarily.",
-        "description": "You can load this unloaded plugin by restarting or using {tr}load cmd. Useful for cases like seting notes in rose bot({tr}unload markdown).",
-        "usage": "{tr}unload <plugin name>",
-        "examples": "{tr}unload markdown",
-    },
-)
-async def unload(event):
-    "To unload a plugin temporarily."
-    shortname = event.pattern_match.group(1)
-    try:
-        remove_plugin(shortname)
-        await edit_or_reply(event, f"Unloaded {shortname} successfully")
-    except Exception as e:
-        await edit_or_reply(event, f"Successfully unload {shortname}\n{str(e)}")
-
-
-@catub.cat_cmd(
-    pattern="uninstall (.*)",
-    command=("uninstall", plugin_category),
+    pattern="الغاء التنصيب (.*)",
+    command=("الغاء التنصيب", plugin_category),
     info={
         "header": "To uninstall a plugin temporarily.",
         "description": "To stop functioning of that plugin and remove that plugin from bot.",
@@ -143,7 +64,7 @@ async def unload(event):
     path = Path(f"userbot/plugins/{shortname}.py")
     if not os.path.exists(path):
         return await edit_delete(
-            event, f"There is no plugin with path {path} to uninstall it"
+            event, f"لا يوجد هكذا ملف مع المسار {path} لحذفه"
         )
     os.remove(path)
     if shortname in CMD_LIST:
@@ -154,6 +75,6 @@ async def unload(event):
         CMD_HELP.pop(shortname)
     try:
         remove_plugin(shortname)
-        await edit_or_reply(event, f"{shortname} is Uninstalled successfully")
+        await edit_or_reply(event, f"{shortname} تم الغاء تثبيت الملف بنجاح")
     except Exception as e:
-        await edit_or_reply(event, f"Successfully uninstalled {shortname}\n{str(e)}")
+        await edit_or_reply(event, f"تم الغاء التثبيت بنجاح {shortname}\n{str(e)}")
