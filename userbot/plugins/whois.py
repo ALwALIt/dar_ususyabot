d72df04fcca6eff000e1e01c91df24d4fa866d2f
JMTHON <85131843+JMTHON-AR@users.noreply.github.com>
2021-06-09 14:11:46 +0300

    Add files via upload

--- userbot/plugins/whois.py
+++ userbot/plugins/whois.py
@@ -1,168 +1,30 @@
-# Userbot module for fetching info about any user on Telegram(including you!).
+# whois code for @Jmthon
 
-import html
 import os
 
-from requests import get
 from telethon.tl.functions.photos import GetUserPhotosRequest
 from telethon.tl.functions.users import GetFullUserRequest
+from telethon.tl.types import MessageEntityMentionName
 from telethon.utils import get_input_location
 
-from userbot import catub
-from userbot.core.logger import logging
-
-from ..Config import Config
-from ..core.managers import edit_or_reply
-from ..helpers import get_user_from_event, reply_id
-from . import spamwatch
-
-plugin_category = "utils"
-LOGS = logging.getLogger(__name__)
+TMP_DOWNLOAD_DIRECTORY = Config.TMP_DOWNLOAD_DIRECTORY
 
 
-async def fetch_info(replied_user, event):
-    """Get details from the User object."""
-    replied_user_profile_photos = await event.client(
-        GetUserPhotosRequest(
-            user_id=replied_user.user.id, offset=42, max_id=0, limit=80
-        )
-    )
-    replied_user_profile_photos_count = "هذا المستخدم لم يضع اي صوره"
-    try:
-        replied_user_profile_photos_count = replied_user_profile_photos.count
-    except AttributeError:
-        pass
-    user_id = replied_user.user.id
-    first_name = replied_user.user.first_name
-    last_name = replied_user.user.last_name
-    try:
-        dc_id, location = get_input_location(replied_user.profile_photo)
-    except Exception:
-        dc_id = "Couldn't fetch DC ID!"
-    common_chat = replied_user.common_chats_count
-    username = replied_user.user.username
-    user_bio = replied_user.about
-    is_bot = replied_user.user.bot
-    restricted = replied_user.user.restricted
-    verified = replied_user.user.verified
-    photo = await event.client.download_profile_photo(
-        user_id,
-        Config.TMP_DOWNLOAD_DIRECTORY + str(user_id) + ".jpg",
-        download_big=True,
-    )
-    first_name = (
-        first_name.replace("\u2060", "")
-        if first_name
-        else ("هذا المستخدم ليس لديه اسم اول")
-    )
-    last_name = last_name.replace("\u2060", "") if last_name else (" ")
-    username = "@{}".format(username) if username else ("لايوجد معرف")
-    user_bio = "لاتوجد نبذه" if not user_bio else user_bio
-    caption = "<b><i> 𓆩JMTHON SOURCE 𓆪 </i></b>\n"
-    caption += f"<b> ⌁ | الأسم ↬</b> {first_name} {last_name}\n"
-    caption += f"<b> ⌁ | الـمعرف ↬</b> {username}\n"
-    caption += f"<b> ⌁ | الايدي ↬</b> <code>{user_id}</code>\n"
-    caption += f"<b> ⌁ | عـدد الصور ↬</b> {replied_user_profile_photos_count}\n"
-    caption += f"<b> ⌁ | النبـذة ↬ </b> {user_bio} \n"
-    caption += f"<b> ⌁ | حـسابه ↬</b> \n"
-    caption += f'<a href="tg://user?id={user_id}">{first_name}</a> \n'
-    caption += f"<b>  الـسورس</b> - [𝗝ََِ𝗠ٓ𝗧َُِْٓ𝗛ُ𝗢َ𝗡ٍَ](t.me/JMTHON) ⌁ "
-    return photo, caption
-
-
-@catub.cat_cmd(
-    pattern="كشف(?: |$)(.*)",
-    command=("كشف", plugin_category),
-    info={
-        "الامر": "امر الكشف يقوم بإرسال بعض معلومات الشهص مم المجموعات المشتركه والخ  ، ",
-        "الشرح": "للحصول على بعض معلومات الشخص.",
-        "الاستخدام": "{tr}كشف <المعرف/ايدي/بالرد عليه>",
-    },
-)
-async def _(event):
-    "Gets information of an user such as restrictions ban by spamwatch or cas"
-    replied_user, error_i_a = await get_user_from_event(event)
-    if not replied_user:
-        return
-    catevent = await edit_or_reply(event, "**يتم حساب المعلومات**")
-    replied_user = await event.client(GetFullUserRequest(replied_user.id))
-    user_id = replied_user.user.id
-    # some people have weird HTML in their names
-    first_name = html.escape(replied_user.user.first_name)
-    # https://stackoverflow.com/a/5072031/4723940
-    # some Deleted Accounts do not have first_name
-    if first_name is not None:
-        # some weird people (like me) have more than 4096 characters in their
-        # names
-        first_name = first_name.replace("\u2060", "")
-    # inspired by https://telegram.dog/afsaI181
-    common_chats = replied_user.common_chats_count
-    try:
-        dc_id, location = get_input_location(replied_user.profile_photo)
-    except Exception:
-        dc_id = "Couldn't fetch DC ID!"
-    if spamwatch:
-        ban = spamwatch.get_ban(user_id)
-        if ban:
-            sw = f"**Spamwatch Banned :** `True` \n       **-**🤷‍♂️**Reason : **`{ban.reason}`"
-        else:
-            sw = f"**Spamwatch Banned :** `False`"
-    else:
-        sw = "**Spamwatch Banned :**`Not Connected`"
-    try:
-        casurl = "https://api.cas.chat/check?user_id={}".format(user_id)
-        data = get(casurl).json()
-    except Exception as e:
-        LOGS.info(e)
-        data = None
-    if data:
-        if data["ok"]:
-            cas = "**Antispam(CAS) Banned :** `True`"
-        else:
-            cas = "**Antispam(CAS) Banned :** `False`"
-    else:
-        cas = "**Antispam(CAS) Banned :** `Couldn't Fetch`"
-    caption = """**معلومات [{}](tg://user?id={}):
-   -🔖 الايدي : **`{}`
-   **-**👥**المجموعات المشتركة : **`{}`
-   **-**🌏**رقم قاعدة البيانات : **`{}`
-   **-**⚕️**الـدعم : **[𝗝ََِ𝗠ٓ𝗧َُِْٓ𝗛ُ𝗢َ𝗡ٍَ](t.me/JMTHON)**
-""".format(
-        first_name,
-        user_id,
-        user_id,
-        common_chats,
-        dc_id,
-        replied_user.user.restricted,
-        sw,
-        cas,
-    )
-    await edit_or_reply(catevent, caption)
-
-
-@catub.cat_cmd(
-    pattern="ايدي(?: |$)(.*)",
-    command=("ايدي", plugin_category),
-    info={
-        "الامر": "Gets info of an user.",
-        "الشرح": "User compelete details.",
-        "الاستخدام": "{tr}whois <username/userid/reply>",
-    },
-)
+@bot.on(admin_cmd(pattern="ايدي(?: |$)(.*)"))
+@bot.on(sudo_cmd(pattern="ايدي(?: |$)(.*)", allow_sudo=True))
 async def who(event):
-    "Gets info of an user"
-    if not os.path.isdir(Config.TMP_DOWNLOAD_DIRECTORY):
-        os.makedirs(Config.TMP_DOWNLOAD_DIRECTORY)
-    replied_user, reason = await get_user_from_event(event)
-    if not replied_user:
-        return
-    cat = await edit_or_reply(event, "**يتم الحصول على المعلومات**")
-    replied_user = await event.client(GetFullUserRequest(replied_user.id))
+    cat = await edit_or_reply(event, "⌁")
+    if not os.path.isdir(TMP_DOWNLOAD_DIRECTORY):
+        os.makedirs(TMP_DOWNLOAD_DIRECTORY)
+    replied_user = await get_user(event)
     try:
         photo, caption = await fetch_info(replied_user, event)
     except AttributeError:
-        return await edit_or_reply(cat, "** لا يمكن الحصول على معلومات هذا المستخدم**")
-    message_id_to_reply = await reply_id(event)
+        await edit_or_reply(cat, "لايمكنني العثور ع المستخدم")
+        return
+    message_id_to_reply = event.message.reply_to_msg_id
+    if not message_id_to_reply:
+        message_id_to_reply = None
     try:
         await event.client.send_file(
             event.chat_id,
@@ -180,20 +42,153 @@ async def who(event):
         await cat.edit(caption, parse_mode="html")
 
 
-@catub.cat_cmd(
-    pattern="رابط الحساب?: |$)(.*)",
-    command=("رابط الحساب", plugin_category),
-    info={
-        "الامر": "يقوم بصنع رابط لحساب اي شخص حتى لو غير معرفه سيبقى الرابط شغال .",
-        "الاستخدام": "{tr}رابط الحساب <معرف/ايدي/بالرد عليه>",
-    },
-)
+async def get_user(event):
+    """ Get the user from argument or replied message. """
+    if event.reply_to_msg_id and not event.pattern_match.group(1):
+        previous_message = await event.get_reply_message()
+        replied_user = await event.client(
+            GetFullUserRequest(previous_message.sender_id)
+        )
+    else:
+        user = event.pattern_match.group(1)
+        if user.isnumeric():
+            user = int(user)
+        if not user:
+            self_user = await event.client.get_me()
+            user = self_user.id
+        if event.message.entities:
+            probable_user_mention_entity = event.message.entities[0]
+            if isinstance(probable_user_mention_entity, MessageEntityMentionName):
+                user_id = probable_user_mention_entity.user_id
+                replied_user = await event.client(GetFullUserRequest(user_id))
+                return replied_user
+        try:
+            user_object = await event.client.get_entity(user)
+            replied_user = await event.client(GetFullUserRequest(user_object.id))
+        except (TypeError, ValueError) as err:
+            await event.edit(str(err))
+            return None
+    return replied_user
+
+
+async def fetch_info(replied_user, event):
+    """ Get details from the User object. """
+    replied_user_profile_photos = await event.client(
+        GetUserPhotosRequest(
+            user_id=replied_user.user.id, offset=42, max_id=0, limit=80
+        )
+    )
+    replied_user_profile_photos_count = "لاتوجد صوره بروفايل"
+    try:
+        replied_user_profile_photos_count = replied_user_profile_photos.count
+    except AttributeError:
+        pass
+    user_id = replied_user.user.id
+    first_name = replied_user.user.first_name
+    last_name = replied_user.user.last_name
+    try:
+        dc_id, location = get_input_location(replied_user.profile_photo)
+    except:
+        pass
+    replied_user.common_chats_count
+    username = replied_user.user.username
+    user_bio = replied_user.about
+    replied_user.user.bot
+    replied_user.user.restricted
+    replied_user.user.verified
+    photo = await event.client.download_profile_photo(
+        user_id, TMP_DOWNLOAD_DIRECTORY + str(user_id) + ".jpg", download_big=True
+    )
+    first_name = (
+        first_name.replace("\u2060", "")
+        if first_name
+        else ("هذا المستخدم ليس له اسم أول")
+    )
+    last_name = last_name.replace("\u2060", "") if last_name else (" ")
+    username = "@{}".format(username) if username else ("لايوجد معرف")
+    user_bio = "لاتوجد نبذه" if not user_bio else user_bio
+    caption = "<b><i> 𓆩JMTHON SOURCE 𓆪 </i></b>\n"
+    caption += f"<b> ⌁ | الأسم ↬</b> {first_name} {last_name}\n"
+    caption += f"<b> ⌁ | الـمعرف ↬</b> {username}\n"
+    caption += f"<b> ⌁ | الايدي ↬</b> <code>{user_id}</code>\n"
+    caption += f"<b> ⌁ | عـدد الصور ↬</b> {replied_user_profile_photos_count}\n"
+    caption += f"<b> ⌁ | النبـذة ↬ </b> {user_bio} \n"
+    caption += f"<b> ⌁ | حـسابه ↬</b> \n"
+    caption += f'<a href="tg://user?id={user_id}">{first_name}</a> \n'
+    caption += f"<b>  Jmthon Source</b> - @JMTHON ⌁ "
+    return photo, caption
+
+
+@bot.on(admin_cmd(pattern="رابط الحساب(?: |$)(.*)"))
+@bot.on(sudo_cmd(pattern="رابط الحساب(?: |$)(.*)", allow_sudo=True))
 async def permalink(mention):
-    """Generates a link to the user's PM with a custom text."""
+    """ For .link command, generates a link to the user's PM with a custom text. """
     user, custom = await get_user_from_event(mention)
     if not user:
         return
     if custom:
-        return await edit_or_reply(mention, f"[{custom}](tg://user?id={user.id})")
-    tag = user.first_name.replace("\u2060", "") if user.first_name else user.username
-    await edit_or_reply(mention, f"[{tag}](tg://user?id={user.id})")
+        await edit_or_reply(
+            mention, f"**  رابط الحساب ** [{custom}](tg://user?id={user.id}) **"
+        )
+    else:
+        tag = (
+            user.first_name.replace("\u2060", "") if user.first_name else user.username
+        )
+        await edit_or_reply(
+            mention, f"** رابط الحساب ** [{tag}](tg://user?id={user.id}) **"
+        )
+
+
+async def get_user_from_event(event):
+    """ Get the user from argument or replied message. """
+    args = event.pattern_match.group(1).split(":", 1)
+    extra = None
+    if event.reply_to_msg_id and len(args) != 2:
+        previous_message = await event.get_reply_message()
+        user_obj = await event.client.get_entity(previous_message.sender_id)
+        extra = event.pattern_match.group(1)
+    elif len(args[0]) > 0:
+        user = args[0]
+        if len(args) == 2:
+            extra = args[1]
+        if user.isnumeric():
+            user = int(user)
+        if not user:
+            await event.edit("`Pass the user's username, id or reply!`")
+            return
+        if event.message.entities:
+            probable_user_mention_entity = event.message.entities[0]
+            if isinstance(probable_user_mention_entity, MessageEntityMentionName):
+                user_id = probable_user_mention_entity.user_id
+                user_obj = await event.client.get_entity(user_id)
+                return user_obj
+        try:
+            user_obj = await event.client.get_entity(user)
+        except (TypeError, ValueError) as err:
+            await event.edit(str(err))
+            return None
+    return user_obj, extra
+
+
+async def ge(user, event):
+    if isinstance(user, str):
+        user = int(user)
+    try:
+        user_obj = await event.client.get_entity(user)
+    except (TypeError, ValueError) as err:
+        await event.edit(str(err))
+        return None
+    return user_obj
+
+
+CMD_HELP.update(
+    {
+        "whois": "**Plugin : **`whois`\
+    \n\n  •  **Syntax : **`.whois <username> or reply to someones text with .whois`\
+    \n  •  **Function : **__Gets info of an user.__\
+    \n\n  •  **Syntax : **`.userinfo <username> or reply to someones text with .userinfo`\
+    \n  •  **Function : **__Gets information of an user such as restrictions ban by spamwatch or cas__\
+    \n\n  •  **Syntax : **`.link id/username/reply`\
+    \n  •  **Function : **__Generates a link to the user's PM .__"
+    }
+)
