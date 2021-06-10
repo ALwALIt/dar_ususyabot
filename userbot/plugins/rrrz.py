06757d03243f1846ce95f453d2156e8f62ac4b5e
JMTHON <85131843+JMTHON-AR@users.noreply.github.com>
2021-06-10 21:07:58 +0300

    Update rrrz.py

--- userbot/plugins/rrrz.py
+++ userbot/plugins/rrrz.py
@@ -8,4 +8,4 @@ import asyncio
 @borg.on(admin_cmd("السورس"))
 async def _(event):
      if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
-        await event.edit("⦑ Welcome to Source ⦒\n\n⌁ ¦ JMTHON TEAM \n┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉\n⌁ ¦ [SOURCE CHANNEL](t.me/JMTHON)\n\n⌁ ¦  [TOOLS CHANNEL](t.me/JMTHON_TOOLS)      \n\n⌁ ¦ [MUHMMAD DEV](T.ME/RRRD7)\n\n⌁ ¦ [JASEM DEV](t.me/UUNZZ)\n ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ \n\n⌁ ¦ [CHAT SUPPORT](T.ME/JMTHON_CHAT)")
+        await event.edit("⦑ مرحبا بك في جمثون ⦒\n\n⌁ ¦ JMTHON TEAM \n┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉\n⌁ ¦ [قناة سورس جمثون](t.me/JMTHON)\n\n⌁ ¦  [قناة مساعدة جمثون](t.me/JMTHON_TOOLS)      \n\n⌁ ¦ [المطور السورس¹](T.ME/RRRD7)\n\n⌁ ¦ [المطور السورس²](t.me/UUNZZ)\n ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ \n\n⌁ ¦ [كروب المساعدة](t.me/JMTHON_CHAT)")
