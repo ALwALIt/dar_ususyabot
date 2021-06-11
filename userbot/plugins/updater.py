5d7faaee37447d87af354465efa06fc603ed6e66
JMTHON <85131843+JMTHON-AR@users.noreply.github.com>
2021-06-12 01:44:32 +0300

    Add files via upload

--- userbot/plugins/updater.py
+++ userbot/plugins/updater.py
@@ -190,7 +190,7 @@ async def deploy(event, repo, ups_rem, ac_br, txt):
 async def upstream(event):
     "To check if the bot is up to date and update if specified"
     conf = event.pattern_match.group(1).strip()
-    event = await edit_or_reply(event, "**التاكد من وجود تحديث ⚕️**")
+    event = await edit_or_reply(event, "**يتم التاكد من وجود تحديث ⚕️**")
     off_repo = UPSTREAM_REPO_URL
     force_update = False
     if HEROKU_API_KEY is None or HEROKU_APP_NAME is None:
@@ -243,7 +243,7 @@ async def upstream(event):
     if changelog == "" and not force_update:
         await event.edit(
             "\n**بـوت جمثون محدث لاخر اصدار ✅❕**  "
-            f"****\n"
+            f""
         )
         return repo.__del__()
     if conf == "" and not force_update:
@@ -268,7 +268,7 @@ async def upstream(event):
 )
 async def upstream(event):
     event = await edit_or_reply(event, "**يتم البحث عن التحديث انتظر**")
-    off_repo = "https://github.com/JMTHON/JMTHON-PACK"
+    off_repo = "https://github.com/JMTHON/JM-THON"
     os.chdir("/app")
     await _catutils.runcmd(f"rm -rf .git")
     try:
@@ -323,4 +323,4 @@ async def variable(var):
         )
     heroku_var = app.config()
     await edit_or_reply(var, f"`Changing goodcat to badcat wait for 2-3 minutes.`")
-    heroku_var["UPSTREAM_REPO"] = "https://github.com/JMTHON-AR/JMTHON"
+    heroku_var["UPSTREAM_REPO"] = "https://github.com/JMTHON-AR/JM-THON"
