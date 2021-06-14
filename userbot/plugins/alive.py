cb6a3b8f43d6af6952520e74a115850b86a7fe5b
JMTHON <85131843+JMTHON-AR@users.noreply.github.com>
2021-06-14 13:14:08 +0300

    Add files via upload

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -1,7 +1,9 @@
+#BY @RRRD7 
 import random
 import re
 import time
 from platform import python_version
+from datetime import datetime
 
 from telethon import version
 from telethon.events import CallbackQuery
@@ -13,40 +15,46 @@ from ..helpers.utils import reply_id
 from ..sql_helper.globals import gvarstatus
 from . import StartTime, catub, catversion, mention
 
+ANIME_QUOTE = [
+    "WELCOME TO JMTHON  BOT",
+]
 plugin_category = "utils"
 
-
+start = datetime.now()
 @catub.cat_cmd(
     pattern="فحص$",
     command=("فحص", plugin_category),
     info={
-        "header": "To check bot's alive status",
+        "header": "To check bot's alive status, Plugin by [∞](https://t.me/Infinity20998),[Λｙａｎ](https://t.me/not_ayan)",
         "options": "To show media in this cmd you need to set ALIVE_PIC with media link, get this by replying the media by .tgm",
         "usage": [
-            "{tr}فحص",
+            "{tr}alive",
         ],
     },
 )
 async def amireallyalive(event):
     "A kind of showing bot details"
+    ANIME = f"{random.choice(ANIME_QUOTE)}"
     reply_to_id = await reply_id(event)
     uptime = await get_readable_time((time.time() - StartTime))
     _, check_sgnirts = check_data_base_heal_th()
-    EMOJI = gvarstatus("ALIVE_EMOJI") or "✧"
-    ALIVE_TEXT = gvarstatus("ALIVE_TEXT") or "⇝ 𝗪َ𝗘𝗟َِ𝗖𝗢𝗠َِ𝙀َِ 𝗧𝗢 𝗝𝗠𝗧𝗛𝗢𝗡 𝄵 ⇜"
-    CAT_IMG = gvarstatus("ALIVE_PIC") or "https://telegra.ph/file/172820c94cdc4f87d5dd1.jpg"
+    EMOJI = gvarstatus("ALIVE_EMOJI") or "✧✧"
+    CUSTOM_ALIVE_TEXT = gvarstatus("ALIVE_TEXT") or ANIME
+    CAT_IMG = gvarstatus("ALIVE_PIC")
     if CAT_IMG:
         CAT = [x for x in CAT_IMG.split()]
         A_IMG = list(CAT)
         PIC = random.choice(A_IMG)
-        cat_caption = f"**{ALIVE_TEXT}**\n\n"
-        cat_caption += f"**{EMOJI} قاعدۿ البيانات :** `{check_sgnirts}`\n"
-        cat_caption += f"**{EMOJI} نسخۿ تليثون :** `{version.__version__}\n`"
+        end = datetime.now()
+        ms = (end - start).microseconds / 1000
+        cat_caption = f"**{CUSTOM_ALIVE_TEXT}**\n\n"
         cat_caption += f"**{EMOJI} نسخـۿ جـمثون:** `{catversion}`\n"
+        cat_caption += f"**{EMOJI} نسخۿ تليثون  :** `{version.__version__}\n`"
         cat_caption += f"**{EMOJI} نسخـۿ البايثون :** `{python_version()}\n`"
-        cat_caption += f"**{EMOJI} الوقت :** `{uptime}\n`"
+        cat_caption += f"**{EMOJI} قاعدۿ البيانات:** `{check_sgnirts}\n`"
         cat_caption += f"**{EMOJI} المنشئ:** {mention}\n"
-        cat_caption += f"**{EMOJI}**  **[𝗦𝗼𝘂𝗿𝗰𝗲 𝗖𝗵𝗮𝗻𝗻𝗲𝗹]**(t.me/jmthon)   .\n"
+        cat_caption += f"**{EMOJI} الوقت :** `{uptime}`\n"
+        cat_caption += f"*{EMOJI} قـناة السـورس: @JMTHON \n" 
         await event.client.send_file(
             event.chat_id,
             PIC,
@@ -59,14 +67,15 @@ async def amireallyalive(event):
         await edit_or_reply(
             event,
             f"**{ALIVE_TEXT}**\n\n"
-            f"**{EMOJI} قاعدۿ البيانات :** `تعمل بنجاح`\n"
-            f"**{EMOJI} نسخۿ تليثون :** `{version.__version__}\n`"
+            f"**{EMOJI} قاعدۿ البيانات :** `{check_sgnirts}`\n"
+            f"**{EMOJI} نسخۿ تليثون  :** `{version.__version__}\n`"
             f"**{EMOJI} نسخـۿ جـمثون :** `{catversion}`\n"
             f"**{EMOJI} نسخـۿ البايثون :** `{python_version()}\n`"
             f"**{EMOJI} الوقت :** `{uptime}\n`"
-            f"**{EMOJI} المنشئ:** {mention}\n"
-            f"**{EMOJI} قنـاة السـورس**: T.ME/JMTHON\n",
+            f"**{EMOJI} المنشئ:** {mention}\n",
         )
+
+
 
 @catub.tgbot.on(CallbackQuery(data=re.compile(b"stats")))
 async def on_plug_in_callback_query_handler(event):
