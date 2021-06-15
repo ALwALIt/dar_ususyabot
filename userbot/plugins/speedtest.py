1dfc7cbcb59b724cbf75eb7907b73c7a5d65d1c2
JMTHON <85131843+JMTHON-AR@users.noreply.github.com>
2021-06-16 00:05:29 +0300

    Update speedtest.py

--- userbot/plugins/speedtest.py
+++ userbot/plugins/speedtest.py
@@ -90,7 +90,7 @@ async def _(event):
             await event.client.send_file(
                 event.chat_id,
                 speedtest_image,
-                caption="**SpeedTest** completed in {} seconds".format(ms),
+                caption="**اختبار السرعة** اكتمل في  {} من الثواني ".format(ms),
                 force_document=as_document,
                 reply_to=reply_msg_id,
                 allow_cache=False,
