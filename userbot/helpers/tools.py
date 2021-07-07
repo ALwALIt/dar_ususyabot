86346d1e45fc0a7ea762a66565276cb04f0d6fe4
JMTHON <85131843+JMTHON-AR@users.noreply.github.com>
2021-07-07 22:30:55 +0300

    -

--- userbot/helpers/tools.py
+++ userbot/helpers/tools.py
@@ -3,32 +3,32 @@ from html_telegraph_poster import TelegraphPoster
 
 def media_type(message):
     if message and message.photo:
-        return "كتابة"
+        return "Photo"
     if message and message.audio:
-        return "اغنية"
+        return "Audio"
     if message and message.voice:
-        return "صوتي"
+        return "Voice"
     if message and message.video_note:
-        return "جولة"
+        return "Round Video"
     if message and message.gif:
-        return "متحركة"
+        return "Gif"
     if message and message.sticker:
-        return "ملصق"
+        return "Sticker"
     if message and message.video:
-        return "فيديو"
+        return "Video"
     if message and message.document:
-        return "ملف"
+        return "Document"
     return None
 
 
 async def post_to_telegraph(page_title, html_format_content):
     post_client = TelegraphPoster(use_api=True)
-    auth_name = "CatUserbot"
+    auth_name = "JMTHON"
     post_client.create_api_token(auth_name)
     post_page = post_client.post(
         title=page_title,
         author=auth_name,
-        author_url="https://t.me/Jmthon",
+        author_url="https://t.me/JMTHON",
         text=html_format_content,
     )
     return post_page["url"]
