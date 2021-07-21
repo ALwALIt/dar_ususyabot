eab33b02f19da362ae915f6e720e2ec513de4dec
JMTHON <85131843+JMTHON-AR@users.noreply.github.com>
2021-07-21 17:55:29 +0300

    Add files via upload

--- stringsetup.py
+++ stringsetup.py
@@ -8,7 +8,10 @@ from telethon.sessions import StringSession
 from telethon.sync import TelegramClient
 
 print(
-    """SOURCE JMTHON - STRING SESSION"""
+    """Please go-to my.telegram.org
+Login using your Telegram account
+Click on API Development Tools
+Create a new application, by entering the required details"""
 )
 APP_ID = int(input("Enter APP ID here: "))
 API_HASH = input("Enter API HASH here: ")
