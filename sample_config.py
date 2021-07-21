a470f967c2f2a7be69e9d00632a8eb4ad214fc5a
JMTHON <85131843+JMTHON-AR@users.noreply.github.com>
2021-07-21 18:41:47 +0300

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -15,7 +15,7 @@ class Config(object):
     # set this value with your name
     ALIVE_NAME = os.environ.get("ALIVE_NAME", None)
     # Get the values for following 2 from my.telegram.org
-    APP_ID = os.environ.get("APP_ID", None)
+    APP_ID = int(os.environ.get("APP_ID", 6))
     API_HASH = os.environ.get("API_HASH") or None
     # Datbase url heroku sets it automatically else get this from elephantsql
     DB_URI = os.environ.get("DATABASE_URL", None)
