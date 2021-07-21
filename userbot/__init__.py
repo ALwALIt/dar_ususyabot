282d660723acb4911535b1219c34ec96e7feccb5
JMTHON <85131843+JMTHON-AR@users.noreply.github.com>
2021-07-21 18:43:45 +0300

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -4,7 +4,7 @@ import heroku3
 
 from .Config import Config
 from .core.logger import logging
-from .core.session import catub
+from .core.session import jmthon
 from .sql_helper.globals import addgvar, delgvar, gvarstatus
 
 __version__ = "2.0.0"
