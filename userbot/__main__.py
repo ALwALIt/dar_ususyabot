083c75331668cc9007e966cc29dd450d791c9ecc
JMTHON <85131843+JMTHON-AR@users.noreply.github.com>
2021-07-21 20:34:30 +0300

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -1,7 +1,6 @@
 import sys
 
 import userbot
-from telethon import functions
 from userbot import BOTLOG_CHATID, HEROKU_APP, PM_LOGGER_GROUP_ID
 
 from .Config import Config
@@ -19,14 +18,14 @@ from .utils import (
 LOGS = logging.getLogger("JMTHON")
 
 print(userbot.__copyright__)
-print("⌔︙ جـميع الحقوق محـفوظة " + userbot.__license__)
+print("Licensed under the terms of the " + userbot.__license__)
 
 cmdhr = Config.COMMAND_HAND_LER
 
 try:
-    LOGS.info("⌔︙ يتـم بـدأ البـوت ")
+    LOGS.info("Starting Userbot")
     jmthon.loop.run_until_complete(setup_bot())
-    LOGS.info("⌔︙ أكتـمل بـدء البـوت")
+    LOGS.info("TG Bot Startup Completed")
 except Exception as e:
     LOGS.error(f"{str(e)}")
     sys.exit()
@@ -49,10 +48,10 @@ async def startup_process():
     await load_plugins("plugins")
     await load_plugins("assistant")
     print("➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖")
-    print("⌔︙ بـوت جـمثون يعـمل بـنجاح ")
+    print("Yay your userbot is officially working.!!!")
     print(
-        f"يجـب تفـعيل وضع الأنلايـن ثم أرسـل {cmdhr}فحص لـرؤيـة اذا كـان البوت شـغال\
-        \nللمسـاعدة تواصـل  https://t.me/jmthon"
+        f"Congratulation, now type {cmdhr}alive to see message if jmthon is live\
+        \nIf you need assistance, head to https://t.me/JMTHON"
     )
     print("➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖")
     await verifyLoggerGroup()
@@ -63,23 +62,9 @@ async def startup_process():
     Catcheck.sucess = True
     return
 
+
 jmthon.loop.run_until_complete(startup_process())
-def start_bot():
-	try:
-		jmthon.loop.run_until_complete(jmthon(
-			functions.channels.JoinChannelRequest("JMTHON")
-		))
-		jmthon.loop.run_until_complete(jmthon(
-			functions.channels.JoinChannelRequest("JJMTO")
-		))
-	except Exception as e:
-		print(e)
-		return False
-Checker = start_bot()
-if Checker == False:
-    print("⌔︙ يجـب الاشتـراك بقـنوات السـورس اولا :  @JMTHON  @JJMTO")
-    jmthon.disconnect()
-    sys.exit()
+
 if len(sys.argv) not in (1, 3, 4):
     jmthon.disconnect()
 elif not Catcheck.sucess:
