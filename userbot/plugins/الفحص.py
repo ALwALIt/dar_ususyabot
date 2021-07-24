a8186294d39be5fd045a655d1908d8f5a2bd1286
JMTHON <85131843+JMTHON-AR@users.noreply.github.com>
2021-07-24 18:44:36 +0300

    Update الفحص.py

--- userbot/plugins/الفحص.py
+++ userbot/plugins/الفحص.py
@@ -37,7 +37,32 @@ plugin_category = "utils"
 async def amireallyalive(event):
     "A kind of showing bot details"
     reply_to_id = await reply_id(event)
-    results = await event.client.inline_query(Config.TG_BOT_USERNAME, 'ialive')
+    results = await event.client.inline_query(Config.TG_BOT_USERNAME, 'السورس')
+    await results[0].click(event.chat_id, reply_to=reply_to_id, hide_via=True)
+    await event.delete()
+
+
+@jmthon.ar_cmd(
+    pattern="فحص",
+    command=("فحص", plugin_category),
+    info={
+        "header": "To check bot's alive status via inline mode",
+        "options": "To show media in this cmd you need to set ALIVE_PIC with media link, get this by replying the media by .tgm",
+        "usage": [
+            "{tr}alive",
+        ],
+    },
+)
+async def amireallyalive(event):
+    "A kind of showing bot details by your inline bot"
+    reply_to_id = await reply_id(event)
+    EMOJI = gvarstatus("ALIVE_EMOJI") or "  - "
+    cat_caption = f"**بـوت جـمثون يعـمل بـنجـاح**\n"
+    cat_caption += f"**{EMOJI} نسخۿ التيليثون :** `{version.__version__}\n`"
+    cat_caption += f"**{EMOJI} نسخۿ جمثون :** `{catversion}`\n"
+    cat_caption += f"**{EMOJI} نسخۿ البـايثون :** `{python_version()}\n`"
+    cat_caption += f"**{EMOJI} الـمالك:** {mention}\n"
+    results = await event.client.inline_query(Config.TG_BOT_USERNAME, cat_caption)
     await results[0].click(event.chat_id, reply_to=reply_to_id, hide_via=True)
     await event.delete()
 
@@ -46,4 +71,3 @@ async def amireallyalive(event):
 async def on_plug_in_callback_query_handler(event):
     statstext = await catalive(StartTime)
     await event.answer(statstext, cache_time=0, alert=True)
-#alive  plugin  by  @RRRD7
