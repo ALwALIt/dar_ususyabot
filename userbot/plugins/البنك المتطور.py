3b651908ca9e91c40f01f8d36bde4769a6929854
JMTHON <85131843+JMTHON-AR@users.noreply.github.com>
2021-07-29 18:33:52 +0300

    Create البنك المتطور.py

---
+++ userbot/plugins/البنك المتطور.py
@@ -0,0 +1,57 @@
+#======================================================================================================================================
+#ping -> edited ping with pic by  @RRRD7
+#كتابة الملف لسورس جمثون فقط ممنوع نسبه لنفسك 
+#تخمط دليل فشلك اخمط وكول اني مطور 😂😂
+
+import os
+import asyncio
+from datetime import datetime
+
+from userbot import jmthon
+from . import hmention, reply_id
+
+"""
+try:
+    from . import PING_PIC, PING_TEXT
+except:
+    pass
+"""
+plugin_category = "tools"
+
+PING_PIC = os.environ.get("PING_PIC") or ("https://telegra.ph/file/502a2c9751c3c06222c51.jpg")
+PING_TEXT = os.environ.get("CUSTOM_PING_TEXT", None) or "مـن لا يتعلم من الماضي لا يرحمه المستقبل  . 🖤"
+
+
+@jmthon.ar_cmd(
+    pattern="بنك$",
+    command=("بنك", plugin_category),
+    info={
+        "header": "امر تجربه البوت اذا يشتغل ارسل  .بنك فقط",
+        "option": "امر بنك المتطور كتابة  @RRRD7",
+        "usage": ["{tr}بنك", ],
+    },
+)
+async def _(event):
+    if event.fwd_from:
+        return
+    reply_to_id = await reply_id(event)
+    start = datetime.now()
+    cat = await edit_or_reply(event, "<b><i>  ❤️⃝⃝⃝⃝⃝⃝⃝⃝⃝⃝⃝⃝⃝⃝⃟✨ البــــنك... 🍀⃝⃝⃟🍂 </b></i>", "html")
+    end = datetime.now()
+    await cat.delete()
+    ms = (end - start).microseconds / 1000
+    if PING_PIC:
+        caption = f"<b><i>{PING_TEXT}<i><b>\n<code>┏━━━━━━━┓\n┃ ✦ {ms}\n┃ ✦ <b>{hmention}</b>\n┗━━━━━━━┛"
+        await event.client.send_file(
+            event.chat_id,
+            PING_PIC,
+            caption=caption,
+            parse_mode="html",
+            reply_to=reply_to_id,
+            link_preview=False,
+            allow_cache=True,
+        )
+    else:
+        await event.edit_or_reply(event, "<code>يجـب اضـافة متـغير `PING_PIC`  اولا  f<code>", "html")
+
+#======================================================================================================================================
