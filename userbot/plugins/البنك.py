9936c2e76a5c28660f5e06f39427b0eb65c9aaa1
JMTHON <85131843+JMTHON-AR@users.noreply.github.com>
2021-07-29 18:33:12 +0300

    Update البنك.py

--- userbot/plugins/البنك.py
+++ userbot/plugins/البنك.py
@@ -7,37 +7,6 @@ from ..core.managers import edit_or_reply
 
 plugin_category = "tools"
 
-
-@jmthon.ar_cmd(
-    pattern="بنك( -a|$)",
-    command=("بنك", plugin_category),
-    info={
-        "header": "check how long it takes to ping your userbot",
-        "flags": {"-a": "average ping"},
-        "usage": ["{tr}ping", "{tr}ping -a"],
-    },
-)
-async def _(event):
-    "To check ping"
-    flag = event.pattern_match.group(1)
-    start = datetime.now()
-    if flag == " -a":
-        catevent = await edit_or_reply(event, "`!....`")
-        await asyncio.sleep(0.3)
-        await catevent.edit("`..!..`")
-        await asyncio.sleep(0.3)
-        await catevent.edit("`....!`")
-        end = datetime.now()
-        tms = (end - start).microseconds / 1000
-        ms = round((tms - 0.6) / 3, 3)
-        await catevent.edit(f"⌔︙  سرعه الاستجابة : `{ms} ms`")
-    else:
-        catevent = await edit_or_reply(event, "Pong!")
-        end = datetime.now()
-        ms = (end - start).microseconds / 1000
-        await catevent.edit(f"⌔︙  سرعه الاستجابة:  `{ms} ms`  ")
-
-
 @jmthon.ar_cmd(
     pattern="بنغ$",
     command=("بنغ", plugin_category),
