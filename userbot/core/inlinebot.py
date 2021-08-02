013cda841daa70be7f14607ee041c2ef78957d2e
JMTHON <85131843+JMTHON-AR@users.noreply.github.com>
2021-08-02 14:36:38 +0300

    Update inlinebot.py

--- userbot/core/inlinebot.py
+++ userbot/core/inlinebot.py
@@ -366,7 +366,7 @@ async def inline_handler(event):  # sourcery no-metrics
                 json.dump(jsondata, open(secret, "w"))
             else:
                 json.dump(newsecret, open(secret, "w"))
-        elif string == "مساعدة":
+        elif string == "help":
             _result = main_menu()
             result = builder.article(
                 title="© JMTHON Help",
