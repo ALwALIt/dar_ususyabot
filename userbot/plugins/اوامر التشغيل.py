e50caef340f0a3e644a18a8d3ba529cf5d1884a7
JMTHON <85131843+JMTHON-AR@users.noreply.github.com>
2021-08-10 06:08:57 +0300

    Update اوامر التشغيل.py

--- userbot/plugins/اوامر التشغيل.py
+++ userbot/plugins/اوامر التشغيل.py
@@ -68,7 +68,7 @@ async def _(event):
     "Shutdowns the bot"
     if BOTLOG:
         await event.client.send_message(BOTLOG_CHATID, "**⌔︙ إيقاف التشغيـل ✕ **\n" "**⌔︙ تـم إيقـاف تشغيـل البـوت بنجـاح ✓**")
-    await edit_or_reply(event, "**⌔︙ جـاري إيقـاف تشغيـل البـوت الآن ..**\n⌔︙  **أعـد تشغيـلي يدويـاً لاحقـاً عـبر هيـروڪو ..**\n⌔︙**سيبقى البـوت متوقفـاً عن العمـل")
+    await edit_or_reply(event, "**⌔︙ جـاري إيقـاف تشغيـل البـوت الآن ..**\n⌔︙  **أعـد تشغيـلي يدويـاً لاحقـاً عـبر هيـروڪو ..**\n⌔︙**سيبقى البـوت متوقفـاً عن العمـل**")
     if HEROKU_APP is not None:
         HEROKU_APP.process_formation()["worker"].scale(0)
     else:
