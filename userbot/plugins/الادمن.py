d48a54ad1b40f11791e933fcb0c85578f0f80ef9
JMTHON <85131843+JMTHON-AR@users.noreply.github.com>
2021-08-10 17:55:57 +0300

    Update الادمن.py

--- userbot/plugins/الادمن.py
+++ userbot/plugins/الادمن.py
@@ -248,3 +248,112 @@ async def endmute(event):
         )
     else:
         await catevent.edit(f"⌔︙ الـمستخدم [{user.first_name}](tg://user?id={user.id})\n ⌔︙ تـم طرده بنجاح ✅ ")
+@jmthon.ar_cmd(
+    pattern="حظر(?:\s|$)([\s\S]*)",
+    command=("حظر", plugin_category),
+    info={
+        "⌔︙ الاستخدام": "يقـوم بـحظر شخـص في الـكروب الءي اسـتخدمت فيـه الامر.",
+        "⌔︙ الشرح": "لحـظر شخـص من الكـروب ومـنعه من الأنـضمام مجـددا\
+            \n⌔︙ تـحتاج الصلاحـيات لـهذا الأمـر.",
+        "⌔︙ الامر": [
+            "{tr}حظر <الايدي/المعرف/بالرد عليه>",
+            "{tr}حظر <الايدي/المعرف/بالرد عليه> <السبب>",
+        ],
+    },
+    groups_only=True,
+    require_admin=True,
+)
+async def _ban_person(event):
+    "⌔︙ لحـظر شخص في كـروب مـعين"
+    user, reason = await get_user_from_event(event)
+    if not user:
+        return
+    if user.id == event.client.uid:
+        return await edit_delete(event, "⌔︙ عـذرا لا تسـتطيع حـظر هـذا الـشـخص")
+    catevent = await edit_or_reply(event, "⌔︙ تـم حـظره بـنجاح")
+    try:
+        await event.client(EditBannedRequest(event.chat_id, user.id, BANNED_RIGHTS))
+    except BadRequestError:
+        return await catevent.edit(NO_PERM)
+    try:
+        reply = await event.get_reply_message()
+        if reply:
+            await reply.delete()
+    except BadRequestError:
+        return await catevent.edit(
+            "⌔︙ ليـس لـدي جـميع الصـلاحيـات لكـن سيـبقى محـظور"
+        )
+    if reason:
+        await catevent.edit(
+            f"⌔︙ المسـتخدم {_format.mentionuser(user.first_name ,user.id)} \n ⌔︙ تـم حـظره بنـجاح !!\n**⌔︙السبب : **`{reason}`"
+        )
+    else:
+        await catevent.edit(
+            f"⌔︙ المسـتخدم {_format.mentionuser(user.first_name ,user.id)} \n ⌔︙ تـم حـظره بنـجاح ✅"
+        )
+    if BOTLOG:
+        if reason:
+            await event.client.send_message(
+                BOTLOG_CHATID,
+                f"⌔︙ الحـظر\
+                \nالمسـتخدم: [{user.first_name}](tg://user?id={user.id})\
+                \nالـدردشـة: {event.chat.title}\
+                \nايدي الكروب(`{event.chat_id}`)\
+                \nالسبـب : {reason}",
+            )
+        else:
+            await event.client.send_message(
+                BOTLOG_CHATID,
+                f"⌔︙ الحـظر\
+                \nالمسـتخدم: [{user.first_name}](tg://user?id={user.id})\
+                \nالـدردشـة: {event.chat.title}\
+                \n ايـدي الكـروب: (`{event.chat_id}`)",
+            )
+
+
+@jmthon.ar_cmd(
+    pattern="الغاء حظر(?:\s|$)([\s\S]*)",
+    command=("الغاء حظر", plugin_category),
+    info={
+        "⌔︙ الأسـتخدام": "يقـوم بـالغاء حـظر الشـخص في الـكروب الذي اسـتخدمت فيـه الامر.",
+        "⌔︙ الشرح": "لألـغاء حـظر شخـص من الكـروب والسـماح له من الأنـضمام مجـددا\
+            \n⌔︙ تـحتاج الصلاحـيات لـهذا الأمـر.",
+        "⌔︙ الأمـر": [
+            "{tr}الغاء حظر <الايدي/المعرف/بالرد عليه>",
+            "{tr}الغاء حظر <الايدي/المعرف/بالرد عليه> <السبب> ",
+        ],
+    },
+    groups_only=True,
+    require_admin=True,
+)
+async def nothanos(event):
+    "⌔︙ لألـغاء الـحظر لـشخص في كـروب مـعين"
+    user, _ = await get_user_from_event(event)
+    if not user:
+        return
+    catevent = await edit_or_reply(event, "⌔︙ جـار الـغاء الـحظر أنتـظر رجـاءا")
+    try:
+        await event.client(EditBannedRequest(event.chat_id, user.id, UNBAN_RIGHTS))
+        await catevent.edit(
+            f"⌔︙ الـمستخدم {_format.mentionuser(user.first_name ,user.id)}\n ⌔︙ تـم الـغاء حـظره بنـجاح "
+        )
+        if BOTLOG:
+            await event.client.send_message(
+                BOTLOG_CHATID,
+                "⌔︙ الـغاء الـحظر \n"
+                f"الـمستخدم: [{user.first_name}](tg://user?id={user.id})\n"
+                f"الـدردشـة: {event.chat.title}(`{event.chat_id}`)",
+            )
+    except UserIdInvalidError:
+        await catevent.edit("⌔︙ يـبدو أن هذه الـعمليـة تم إلغاؤهـا")
+    except Exception as e:
+        await catevent.edit(f"**خـطأ :**\n`{e}`")
+
+
+@jmthon.ar_cmd(incoming=True)
+async def watcher(event):
+    if is_muted(event.sender_id, event.chat_id):
+        try:
+            await event.delete()
+        except Exception as e:
+            LOGS.info(str(e))
