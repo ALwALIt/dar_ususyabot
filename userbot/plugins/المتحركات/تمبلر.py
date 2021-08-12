431d406a0ad6317ae37e8325ba5ad77df00150a9
JMTHON <85131843+JMTHON-AR@users.noreply.github.com>
2021-08-12 22:37:13 +0300

    Create تمبلر.py

---
+++ userbot/plugins/المتحركات/تمبلر.py
@@ -0,0 +1,160 @@
+#edit  ~ @RR9R7 for Jmthon 
+
+from telethon import events
+from userbot.utils import admin_cmd
+from userbot import jmthon
+from . import *
+
+#edit  ~ @RR9R7 for Jmthon 
+#جميع الحقوق محفوظة لسـورس جـمثون تخـمط تبيـن فشلـك
+#━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━#
+
+RR9R7 = "JMTHON"            #تمبلر
+tmb = "https://telegra.ph/file/bf2d2cb360b0bc05dd7c2.mp4"
+tmb2 = "https://telegra.ph/file/17c92862ca1283914a91f.mp4"
+tmb3 = "https://telegra.ph/file/89dc704a356b4b0f6fe89.mp4"
+tmb4 = "https://telegra.ph/file/cf43d80cd5bbe0fdf3b5a.mp4"
+tmb5 = "https://telegra.ph/file/8c9d1d7c1368631e9178b.mp4"
+tmb6 = "https://telegra.ph/file/f67f3c36091543134bfe3.mp4"
+tmb7 = "https://telegra.ph/file/1199679fb6b90ff012b7a.mp4"
+
+#━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━#
+
+
+plugin_category = "extra"
+@jmthon.ar_cmd(
+    pattern="ت1$",
+    command=("ت1", plugin_category),
+           )
+async def tmgif(event):
+    if event.fwd_from:
+        return
+    RR9R7 = await reply_id(event)
+    if tmb:
+        jmthon = f"**{RR9R7}**\n"
+        jmthon += f"✛━━━━━━━━━━━━━━━━━━━━✛\n"
+        jmthon += f"**الـمتحـرڪـة الأولـى **\n"
+        jmthon += f"✛━━━━━━━━━━━━━━━━━━━━✛"
+        await event.client.send_file(event.chat_id, tmb, caption=jmthon, reply_to=RR9R7)
+
+#edit  ~ @RR9R7 for Jmthon 
+#جميع الحقوق محفوظة لسـورس جـمثون تخـمط تبيـن فشلـك
+
+@jmthon.ar_cmd(
+    pattern="ت2$",
+    command=("ت2", plugin_category),
+           )
+async def tmgif(event):
+    if event.fwd_from:
+        return
+    leo = await reply_id(event)
+    if tmb2:
+        RAZAN = f"**{RR9R7}**\n"
+        RAZAN += f"✛━━━━━━━━━━━━━━━━━━━━✛\n"
+        RAZAN += f"**الـمتحـرڪـة الـثـانيـة **\n"
+        RAZAN += f"✛━━━━━━━━━━━━━━━━━━━━✛"
+        await event.client.send_file(event.chat_id, tmb2, caption=RAZAN, reply_to=leo)
+
+#edit  ~ @RR9R7 for Jmthon 
+#جميع الحقوق محفوظة لسـورس جـمثون تخـمط تبيـن فشلـك
+
+@jmthon.ar_cmd(
+    pattern="ت3$",
+    command=("ت3", plugin_category),
+           )
+async def tmgif(event):
+    if event.fwd_from:
+        return
+    sic_id = await reply_id(event)
+    if tmb3:
+        RAZAN = f"**{RR9R7}**\n"
+        RAZAN += f"✛━━━━━━━━━━━━━━━━━━━━✛\n"
+        RAZAN += f"**الـمتحـرڪـة الـثـالثـة**\n"
+        RAZAN += f"✛━━━━━━━━━━━━━━━━━━━━✛"
+        await event.client.send_file(event.chat_id, tmb3, caption=RAZAN, reply_to=sic_id)
+
+#edit  ~ @RR9R7 for Jmthon 
+#جميع الحقوق محفوظة لسـورس جـمثون تخـمط تبيـن فشلـك
+
+@jmthon.ar_cmd(
+    pattern="ت4$",
+    command=("ت4", plugin_category),
+           )
+async def tmgif(event):
+    if event.fwd_from:
+        return
+    reply_to_id = await reply_id(event)
+    if tmb4:
+        RAZAN = f"**{RR9R7}**\n"
+        RAZAN += f"✛━━━━━━━━━━━━━━━━━━━━✛\n"
+        RAZAN += f"**الـمتحـرڪـة الـرابـعـة**\n"
+        RAZAN += f"✛━━━━━━━━━━━━━━━━━━━━✛"
+        await event.client.send_file(
+            event.chat_id, tmb4, caption=RAZAN, reply_to=reply_to_id
+        )
+
+#edit  ~ @RR9R7 for Jmthon 
+#جميع الحقوق محفوظة لسـورس جـمثون تخـمط تبيـن فشلـك
+
+@jmthon.ar_cmd(
+    pattern="ت5$",
+    command=("ت5", plugin_category),
+           )
+
+async def tmgif(event):
+    if event.fwd_from:
+        return
+    reply_to_id = await reply_id(event)
+    if tmb5:
+        RAZAN = f"**{RR9R7}**\n"
+        RAZAN += f"✛━━━━━━━━━━━━━━━━━━━━✛\n"
+        RAZAN += f"**الـمتحـرڪـة الـخامسـة**\n"
+        RAZAN += f"✛━━━━━━━━━━━━━━━━━━━━✛"
+        await event.client.send_file(
+            event.chat_id, tmb5, caption=RAZAN, reply_to=reply_to_id
+        )
+
+#edit  ~ @RR9R7 for Jmthon 
+#جميع الحقوق محفوظة لسـورس جـمثون تخـمط تبيـن فشلـك
+
+@jmthon.ar_cmd(
+    pattern="ت6$",
+    command=("ت6", plugin_category),
+           )
+
+async def tmgif(event):
+    if event.fwd_from:
+        return
+    reply_to_id = await reply_id(event)
+    if tmb6:
+        RAZAN = f"**{RR9R7}**\n"
+        RAZAN += f"✛━━━━━━━━━━━━━━━━━━━━✛\n"
+        RAZAN += f"**الـمتحـرڪـة الـسادسـة**\n"
+        RAZAN += f"✛━━━━━━━━━━━━━━━━━━━━✛"
+        await event.client.send_file(
+            event.chat_id, tmb6, caption=RAZAN, reply_to=reply_to_id
+        )
+
+#edit  ~ @RR9R7 for Jmthon 
+#جميع الحقوق محفوظة لسـورس جـمثون تخـمط تبيـن فشلـك
+
+@jmthon.ar_cmd(
+    pattern="ت7$",
+    command=("ت7", plugin_category),
+           )
+async def tmgif(event):
+    if event.fwd_from:
+        return
+    reply_to_id = await reply_id(event)
+    if tmb7:
+        RAZAN = f"**{RR9R7}**\n"
+        RAZAN += f"✛━━━━━━━━━━━━━━━━━━━━✛\n"
+        RAZAN += f"**الـمتحـرڪـة الـسـابعـة**\n"
+        RAZAN += f"✛━━━━━━━━━━━━━━━━━━━━✛"
+        await event.client.send_file(
+            event.chat_id, tmb7, caption=RAZAN, reply_to=reply_to_id
+        )
+    
+       
+#edit  ~ @RR9R7 for Jmthon 
+#جميع الحقوق محفوظة لسـورس جـمثون تخـمط تبيـن فشلـك   
