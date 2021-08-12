9bec2aab30a59c2cc3e16dfefda0f517d0126378
JMTHON <85131843+JMTHON-AR@users.noreply.github.com>
2021-08-12 22:30:36 +0300

    Update متحركات تمبلر.py

--- userbot/plugins/متحركات تمبلر.py
+++ userbot/plugins/متحركات تمبلر.py
@@ -7,6 +7,19 @@ from . import *
 
 #edit  ~ @RR9R7 for Jmthon 
 #جميع الحقوق محفوظة لسـورس جـمثون تخـمط تبيـن فشلـك
+#━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━#
+
+RR9R7 = "JMTHON"            #تمبلر
+tmb = "https://telegra.ph/file/bf2d2cb360b0bc05dd7c2.mp4"
+tmb2 = "https://telegra.ph/file/17c92862ca1283914a91f.mp4"
+tmb3 = "https://telegra.ph/file/89dc704a356b4b0f6fe89.mp4"
+tmb4 = "https://telegra.ph/file/cf43d80cd5bbe0fdf3b5a.mp4"
+tmb5 = "https://telegra.ph/file/8c9d1d7c1368631e9178b.mp4"
+tmb6 = "https://telegra.ph/file/f67f3c36091543134bfe3.mp4"
+tmb7 = "https://telegra.ph/file/1199679fb6b90ff012b7a.mp4"
+
+#━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━#
+
 
 plugin_category = "extra"
 @jmthon.ar_cmd(
