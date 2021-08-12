0fc08e99d77beac2e8b56fce8e8b7c7daaf8d6ef
JMTHON <85131843+JMTHON-AR@users.noreply.github.com>
2021-08-12 22:19:22 +0300

    Add files via upload

---
+++ userbot/plugins/المتحركات/متحركات تحشيش.py
@@ -0,0 +1,134 @@
+#edit  ~ @RR9R7 for Jmthon 
+
+from telethon import events
+from userbot.utils import admin_cmd
+from userbot import jmthon
+from . import *
+
+plugin_category = "extra"
+@jmthon.ar_cmd(
+    pattern="ح1$",
+    command=("ح1", plugin_category),
+           )
+async def tmgif(event):
+    if event.fwd_from:
+        return
+    RR9R7 = await reply_id(event)
+    if fun:
+        jmthon = f"**{RR9R7}**\n"
+        jmthon += f"✛━━━━━━━━━━━━━✛\n"
+        jmthon += f"**الـمتحـرڪـة الأولـى **\n"
+        jmthon += f"✛━━━━━━━━━━━━━✛"
+        await event.client.send_file(event.chat_id, fun, caption=jmthon, reply_to=RR9R7)
+
+#edit  ~ @RR9R7 for Jmthon 
+@jmthon.ar_cmd(
+    pattern="ح2$",
+    command=("ح2", plugin_category),
+           )
+async def tmgif(event):
+    if event.fwd_from:
+        return
+    leo = await reply_id(event)
+    if fun2:
+        RR9R7 = f"**{RR9R7}**\n"
+        RR9R7 += f"✛━━━━━━━━━━━━━✛\n"
+        RR9R7 += f"**الـمتحـرڪـة الـثـانيـة **\n"
+        RR9R7 += f"✛━━━━━━━━━━━━━✛"
+        await event.client.send_file(event.chat_id, fun2, caption=RR9R7, reply_to=leo)
+#edit  ~ @RR9R7 for Jmthon 
+
+@jmthon.ar_cmd(
+    pattern="ح3$",
+    command=("ح3", plugin_category),
+           )
+async def tmgif(event):
+    if event.fwd_from:
+        return
+    sic_id = await reply_id(i)
+    if fun3:
+        RAZAN = f"**{RR9R7}**\n"
+        RAZAN += f"✛━━━━━━━━━━━━━✛\n"
+        RAZAN += f"**الـمتحـرڪـة الـثـالثـة**\n"
+        RAZAN += f"✛━━━━━━━━━━━━━✛"
+        await event.client.send_file(event.chat_id, fun3, caption=RAZAN, reply_to=sic_id)
+#edit  ~ @RR9R7 for Jmthon 
+
+@jmthon.ar_cmd(
+    pattern="ح4$",
+    command=("ح4", plugin_category),
+           )
+async def tmgif(event):
+    if event.fwd_from:
+        return
+    reply_to_id = await reply_id(event)
+    if fun4:
+        RAZAN = f"**{RR9R7}**\n"
+        RAZAN += f"✛━━━━━━━━━━━━━✛\n"
+        RAZAN += f"**الـمتحـرڪـة الـرابـعـة**\n"
+        RAZAN += f"✛━━━━━━━━━━━━━✛"
+        await event.client.send_file(
+            event.chat_id, fun4, caption=RAZAN, reply_to=reply_to_id
+        )
+
+#edit  ~ @RR9R7 for Jmthon 
+@jmthon.ar_cmd(
+    pattern="ح5$",
+    command=("ح5", plugin_category),
+           )
+
+async def tmgif(event):
+    if event.fwd_from:
+        return
+    reply_to_id = await reply_id(event)
+    if fun5:
+        RAZAN = f"**{RR9R7}**\n"
+        RAZAN += f"✛━━━━━━━━━━━━━✛\n"
+        RAZAN += f"**الـمتحـرڪـة الـخامسـة**\n"
+        RAZAN += f"✛━━━━━━━━━━━━━✛"
+        await event.client.send_file(
+            event.chat_id, fun5, caption=RAZAN, reply_to=reply_to_id
+        )
+
+#edit  ~ @RR9R7 for Jmthon 
+
+@jmthon.ar_cmd(
+    pattern="ح6$",
+    command=("ح6", plugin_category),
+           )
+
+async def tmgif(event):
+    if event.fwd_from:
+        return
+    reply_to_id = await reply_id(event)
+    if fun6:
+        RAZAN = f"**{RR9R7}**\n"
+        RAZAN += f"✛━━━━━━━━━━━━━✛\n"
+        RAZAN += f"**الـمتحـرڪـة الـسادسـة**\n"
+        RAZAN += f"✛━━━━━━━━━━━━━✛"
+        await event.client.send_file(
+            event.chat_id, fun6, caption=RAZAN, reply_to=reply_to_id
+        )
+
+#edit  ~ @RR9R7 for Jmthon 
+
+@jmthon.ar_cmd(
+    pattern="ح7$",
+    command=("ح7", plugin_category),
+           )
+async def tmgif(event):
+    if event.fwd_from:
+        return
+    reply_to_id = await reply_id(event)
+    if fun7:
+        RAZAN = f"**{RR9R7}**\n"
+        RAZAN += f"✛━━━━━━━━━━━━━✛\n"
+        RAZAN += f"**الـمتحـرڪـة الـسـابعـة**\n"
+        RAZAN += f"✛━━━━━━━━━━━━━✛"
+        await event.client.send_file(
+            event.chat_id, fun7, caption=RAZAN, reply_to=reply_to_id
+        )
+
+  
+#edit  ~ @RR9R7 for Jmthon 
+#جميع الحقوق محفوظة لسـورس جـمثون تخـمط تبيـن فشلـك
---
+++ userbot/plugins/المتحركات/متحركات تمبلر.py
@@ -0,0 +1,147 @@
+#edit  ~ @RR9R7 for Jmthon 
+
+from telethon import events
+from userbot.utils import admin_cmd
+from userbot import jmthon
+from . import *
+
+#edit  ~ @RR9R7 for Jmthon 
+#جميع الحقوق محفوظة لسـورس جـمثون تخـمط تبيـن فشلـك
+
+plugin_category = "extra"
+@jmthon.ar_cmd(
+    pattern="ت1$",
+    command=("ت1", plugin_category),
+           )
+async def tmgif(event):
+    if event.fwd_from:
+        return
+    RR9R7 = await reply_id(event)
+    if tmb:
+        jmthon = f"**{RR9R7}**\n"
+        jmthon += f"✛━━━━━━━━━━━━━✛\n"
+        jmthon += f"**الـمتحـرڪـة الأولـى **\n"
+        jmthon += f"✛━━━━━━━━━━━━━✛"
+        await event.client.send_file(event.chat_id, tmb, caption=jmthon, reply_to=RR9R7)
+
+#edit  ~ @RR9R7 for Jmthon 
+#جميع الحقوق محفوظة لسـورس جـمثون تخـمط تبيـن فشلـك
+
+@jmthon.ar_cmd(
+    pattern="ت2$",
+    command=("ت2", plugin_category),
+           )
+async def tmgif(event):
+    if event.fwd_from:
+        return
+    leo = await reply_id(event)
+    if tmb2:
+        RR9R7 = f"**{RR9R7}**\n"
+        RR9R7 += f"✛━━━━━━━━━━━━━✛\n"
+        RR9R7 += f"**الـمتحـرڪـة الـثـانيـة **\n"
+        RR9R7 += f"✛━━━━━━━━━━━━━✛"
+        await event.client.send_file(event.chat_id, tmb2, caption=RR9R7, reply_to=leo)
+
+#edit  ~ @RR9R7 for Jmthon 
+#جميع الحقوق محفوظة لسـورس جـمثون تخـمط تبيـن فشلـك
+
+@jmthon.ar_cmd(
+    pattern="ت3$",
+    command=("ت3", plugin_category),
+           )
+async def tmgif(event):
+    if event.fwd_from:
+        return
+    sic_id = await reply_id(i)
+    if tmb3:
+        RAZAN = f"**{RR9R7}**\n"
+        RAZAN += f"✛━━━━━━━━━━━━━✛\n"
+        RAZAN += f"**الـمتحـرڪـة الـثـالثـة**\n"
+        RAZAN += f"✛━━━━━━━━━━━━━✛"
+        await event.client.send_file(event.chat_id, tmb3, caption=RAZAN, reply_to=sic_id)
+
+#edit  ~ @RR9R7 for Jmthon 
+#جميع الحقوق محفوظة لسـورس جـمثون تخـمط تبيـن فشلـك
+
+@jmthon.ar_cmd(
+    pattern="ت4$",
+    command=("ت4", plugin_category),
+           )
+async def tmgif(event):
+    if event.fwd_from:
+        return
+    reply_to_id = await reply_id(event)
+    if tmb4:
+        RAZAN = f"**{RR9R7}**\n"
+        RAZAN += f"✛━━━━━━━━━━━━━✛\n"
+        RAZAN += f"**الـمتحـرڪـة الـرابـعـة**\n"
+        RAZAN += f"✛━━━━━━━━━━━━━✛"
+        await event.client.send_file(
+            event.chat_id, tmb4, caption=RAZAN, reply_to=reply_to_id
+        )
+
+#edit  ~ @RR9R7 for Jmthon 
+#جميع الحقوق محفوظة لسـورس جـمثون تخـمط تبيـن فشلـك
+
+@jmthon.ar_cmd(
+    pattern="ت5$",
+    command=("ت5", plugin_category),
+           )
+
+async def tmgif(event):
+    if event.fwd_from:
+        return
+    reply_to_id = await reply_id(event)
+    if tmb5:
+        RAZAN = f"**{RR9R7}**\n"
+        RAZAN += f"✛━━━━━━━━━━━━━✛\n"
+        RAZAN += f"**الـمتحـرڪـة الـخامسـة**\n"
+        RAZAN += f"✛━━━━━━━━━━━━━✛"
+        await event.client.send_file(
+            event.chat_id, tmb5, caption=RAZAN, reply_to=reply_to_id
+        )
+
+#edit  ~ @RR9R7 for Jmthon 
+#جميع الحقوق محفوظة لسـورس جـمثون تخـمط تبيـن فشلـك
+
+@jmthon.ar_cmd(
+    pattern="ت6$",
+    command=("ت6", plugin_category),
+           )
+
+async def tmgif(event):
+    if event.fwd_from:
+        return
+    reply_to_id = await reply_id(event)
+    if tmb6:
+        RAZAN = f"**{RR9R7}**\n"
+        RAZAN += f"✛━━━━━━━━━━━━━✛\n"
+        RAZAN += f"**الـمتحـرڪـة الـسادسـة**\n"
+        RAZAN += f"✛━━━━━━━━━━━━━✛"
+        await event.client.send_file(
+            event.chat_id, tmb6, caption=RAZAN, reply_to=reply_to_id
+        )
+
+#edit  ~ @RR9R7 for Jmthon 
+#جميع الحقوق محفوظة لسـورس جـمثون تخـمط تبيـن فشلـك
+
+@jmthon.ar_cmd(
+    pattern="ت7$",
+    command=("ت7", plugin_category),
+           )
+async def tmgif(event):
+    if event.fwd_from:
+        return
+    reply_to_id = await reply_id(event)
+    if tmb7:
+        RAZAN = f"**{RR9R7}**\n"
+        RAZAN += f"✛━━━━━━━━━━━━━✛\n"
+        RAZAN += f"**الـمتحـرڪـة الـسـابعـة**\n"
+        RAZAN += f"✛━━━━━━━━━━━━━✛"
+        await event.client.send_file(
+            event.chat_id, tmb7, caption=RAZAN, reply_to=reply_to_id
+        )
+    
+       
+#edit  ~ @RR9R7 for Jmthon 
+#جميع الحقوق محفوظة لسـورس جـمثون تخـمط تبيـن فشلـك   
