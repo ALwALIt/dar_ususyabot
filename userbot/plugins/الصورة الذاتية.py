ee0682c518364ae77e901d8121c978a5f2895c36
- Muhammad ALZUHAIRY <87212030+RR9R7@users.noreply.github.com>
2021-08-17 19:24:29 +0300

    Update الصورة الذاتية.py

--- userbot/plugins/الصورة الذاتية.py
+++ userbot/plugins/الصورة الذاتية.py
@@ -7,11 +7,14 @@ async def oho(event):
     return await event.edit('يجـب عـليك الـرد عـلى صـورة ذاتيـة الـتدمير')
   rr9r7 = await event.get_reply_message()
   pic = await rr9r7.download_media()
-  await bot.send_file(event.chat_id, pic, caption=f"""
+  await bot.send_file(BOTLOG_CHATID, pic, caption=f"""
 -تـم جـلب الصـورة بنجـاح ✅
+- CH: @Jmthon
+- Dev: @RR9R7
   """)
   await event.delete()
 
 #اخمط وسمي روحك مطور ما اوصيك
 # كتابة محمد الزهيري  
 #@RR9R7
+# تعديل  :  @x3raqe
