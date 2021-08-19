0574f3e69a5c103cd849bef2d1069b4ecc503a5b
- Muhammad ALZUHAIRY <87212030+RR9R7@users.noreply.github.com>
2021-08-19 16:43:54 +0300

    - lol

---
+++ userbot/plugins/نسبة الانوثة.py
@@ -0,0 +1,52 @@
+import html
+import os
+import random
+
+from requests import get
+from telethon.tl.functions.photos import GetUserPhotosRequest
+from telethon.tl.functions.users import GetFullUserRequest
+from telethon.utils import get_input_location
+
+from userbot import jmthon
+
+from ..Config import Config
+from ..core.managers import edit_or_reply
+from ..helpers import get_user_from_event, reply_id
+from . import spamwatch
+
+plugin_category = "utils"
+
+
+
+#كـتابة المـلف وتعديل.    :   محـمد الـزهيري.   اخمط وسمي روحك مطور فرخي 😂
+# اذا انت ابن حرام اخمط 😂
+# اي بعدك تريد تخمط ترا من تخمط مراح تنجح
+
+rr7 = [
+    "100%",
+    "90%",
+    "80%",
+    "70%",
+    "60%",
+    "50%",
+    "40%",
+    "30%",
+    "20%",
+    "10%",
+    "0%",
+]
+
+@jmthon.ar_cmd(
+    pattern="نسبة الانوثة(?:\s|$)([\s\S]*)",
+    command=("نسبة الانوثة", plugin_category),
+)
+async def permalink(mention):
+    """Generates a link to the user's PM with a custom text."""
+    user, custom = await get_user_from_event(mention)
+    if not user:
+        return
+    if custom:
+        return await edit_or_reply(mention, f"[{custom}](tg://user?id={user.id})")
+    tag = user.first_name.replace("\u2060", "") if user.first_name else user.username
+    sos = random.choice(rr7)
+    await edit_or_reply(mention, f"⌔︙ نسبة الانوثة لـ [{tag}](tg://user?id={user.id}) هـي {sos} 🥵🖤")
