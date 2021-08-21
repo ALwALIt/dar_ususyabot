2c1846cae3925ac5be8a0650b5d21e2d410e5c77
- Muhammad ALZUHAIRY <87212030+RR9R7@users.noreply.github.com>
2021-08-21 18:48:15 +0300

    Update تلكراف.py

--- userbot/plugins/تلكراف.py
+++ userbot/plugins/تلكراف.py
@@ -29,48 +29,63 @@ def resize_image(image):
 
 
 @jmthon.ar_cmd(
-    pattern="(ت(ل)?ك(راف)?) ?(م|ن|ميديا|نص)(?:\s|$)([\s\S]*)",
-    command=("تلكراف", plugin_category),
-    )
+    pattern="(t(ele)?g(raph)?) ?(m|t|media|text)(?:\s|$)([\s\S]*)",
+    command=("telegraph", plugin_category),
+    info={
+        "header": "To get telegraph link.",
+        "description": "Reply to text message to paste that text on telegraph you can also pass input along with command \
+            So that to customize title of that telegraph and reply to media file to get sharable link of that media(atmost 5mb is supported)",
+        "options": {
+            "m or media": "To get telegraph link of replied sticker/image/video/gif.",
+            "t or text": "To get telegraph link of replied text you can use custom title.",
+        },
+        "usage": [
+            "{tr}tgm",
+            "{tr}tgt <title(optional)>",
+            "{tr}telegraph media",
+            "{tr}telegraph text <title(optional)>",
+        ],
+    },
+)  # sourcery no-metrics
 async def _(event):
-    "To get telegraph link"
-    catevent = await edit_or_reply(event, "**⌔︙ جاري المعالجـة **")
+    "To get telegraph link."
+    catevent = await edit_or_reply(event, "`processing........`")
     if BOTLOG:
         await event.client.send_message(
             BOTLOG_CHATID,
-            f"** تـم صـنع رابـط تليجـراف جديـد  :** {auth_url} \n **للجلسـة الحاليـة، لا تقـم بإعطـاء هـذا الرابـط إلى أي أحـد، حتى وإن قـال بأنّـه موظـف لـدى تليڪـرام !**",
+            f"Created New Telegraph account {auth_url} for the current session. \n**Do not give this url to anyone, even if they say they are from Telegram!**",
         )
     optional_title = event.pattern_match.group(5)
     if not event.reply_to_msg_id:
         return await catevent.edit(
-            "**⌔︙ قـم بالـردّ على رسالـة للحصـول على رابـط صـورة تليجـراف**",
+            "`Reply to a message to get a permanent telegra.ph link.`",
         )
 
     start = datetime.now()
     r_message = await event.get_reply_message()
     input_str = (event.pattern_match.group(4)).strip()
-    if input_str in ["ميديا", "م"]:
+    if input_str in ["media", "m"]:
         downloaded_file_name = await event.client.download_media(
             r_message, Config.TEMP_DIR
         )
-        await catevent.edit(f"**⌔︙ تـم التحميـل إلى**  `{downloaded_file_name}`")
+        await catevent.edit(f"`Downloaded to {downloaded_file_name}`")
         if downloaded_file_name.endswith((".webp")):
             resize_image(downloaded_file_name)
         try:
             media_urls = upload_file(downloaded_file_name)
         except exceptions.TelegraphException as exc:
-            await catevent.edit(f"**⌔︙ حـدث خـطأ مـا : **\n`{str(exc)}`")
+            await catevent.edit(f"**Error : **\n`{exc}`")
             os.remove(downloaded_file_name)
         else:
             end = datetime.now()
             ms = (end - start).seconds
             os.remove(downloaded_file_name)
             await catevent.edit(
-                 f"**⌔︙ الرابـط  : ** [اضغط هنا](https://telegra.ph{media_urls[0]})\
-                    \n**⌔︙ الوقـت المستغـرق   : ** `{ms} الثوانـي.`",
+                f"**link : **[telegraph](https://telegra.ph{media_urls[0]})\
+                    \n**Time Taken : **`{ms} seconds.`",
                 link_preview=True,
             )
-    elif input_str in ["نص", "ن"]:
+    elif input_str in ["text", "t"]:
         user_object = await event.client.get_entity(r_message.sender_id)
         title_of_page = get_display_name(user_object)
         # apparently, all Users do not have last_name field
@@ -103,7 +118,7 @@ async def _(event):
         ms = (end - start).seconds
         cat = f"https://telegra.ph/{response['path']}"
         await catevent.edit(
-            f"**⌔︙ الرابـط  : **[اضغـط هـنا]({cat})\
-                 \n**⌔︙ الوقـت المستغـرق   : ** `{ms} الثوانـي.`",
+            f"**link : ** [telegraph]({cat})\
+                 \n**Time Taken : **`{ms} seconds.`",
             link_preview=True,
         )
