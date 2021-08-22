0a09abb59cfdbc764aa884e798ac1ddfd8375665
- Muhammad ALZUHAIRY <87212030+RR9R7@users.noreply.github.com>
2021-08-22 23:15:57 +0300

    ㅤ
    ㅤ

--- Jmthon/razan/_gif.py
+++ Jmthon/razan/_gif.py
@@ -1,1 +1,16 @@
 #soon
+
+
+#━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━#
+
+RR9R7 = "˛ 𝖩𝗆𝗍𝖧ٰ𝗈𝗇 ، ٰ𝖦𝗂𝖿 𝖲ِ𝖺ٰ𝖣 . ."            #ساد
+sad = "https://telegra.ph/file/4a59106d0ade82859136b.mp4"
+sad2 = "https://telegra.ph/file/6e6f59bac38be6b983f56.mp4"
+sad3 = "https://telegra.ph/file/fa1d3bf72e1f7a1e08474.mp4"
+sad4 = "https://telegra.ph/file/890a01ea6cb065a7faa42.mp4"
+sad5 = "https://telegra.ph/file/66778d145442699ce6038.mp4"
+sad6 = "https://telegra.ph/file/50d9d6481803e21a30716.mp4"
+sad7 = "https://telegra.ph/file/f79a31446027f741715a8.mp4"
+sad8 = "https://telegra.ph/file/d1face4210bb7a883fdef.mp4"
+
+#━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━#
