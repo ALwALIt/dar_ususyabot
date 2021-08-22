6f8ad77a6f4039486598b9d95974f15a82651947
- Muhammad ALZUHAIRY <87212030+RR9R7@users.noreply.github.com>
2021-08-23 01:23:14 +0300

    Update المطور.py

--- userbot/plugins/المطور.py
+++ userbot/plugins/المطور.py
@@ -11,7 +11,7 @@ from telethon.errors.rpcerrorlist import (
 )
 from telethon.events import CallbackQuery
 
-from userbot import StartTime, jmthon, catversion
+from userbot import StartTime, jmthon, JMVERSION
 
 from ..Config import Config
 from ..core.managers import edit_or_reply
