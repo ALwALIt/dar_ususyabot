11f5621c706b45b4874516757467db5e781b6212
- Muhammad ALZUHAIRY <87212030+RR9R7@users.noreply.github.com>
2021-08-24 03:00:38 +0300

    Update متحركات تمبلر.py

--- userbot/plugins/متحركات تمبلر.py
+++ userbot/plugins/متحركات تمبلر.py
@@ -3,22 +3,11 @@
 from telethon import events
 from userbot.utils import admin_cmd
 from userbot import jmthon
+from Jmthon.razan._gif import *
 from . import *
 
 #edit  ~ @RR9R7 for Jmthon 
 #جميع الحقوق محفوظة لسـورس جـمثون تخـمط تبيـن فشلـك
-#━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━#
-
-RR9R7 = "JMTHON"            #تمبلر
-tmb = "https://telegra.ph/file/bf2d2cb360b0bc05dd7c2.mp4"
-tmb2 = "https://telegra.ph/file/17c92862ca1283914a91f.mp4"
-tmb3 = "https://telegra.ph/file/89dc704a356b4b0f6fe89.mp4"
-tmb4 = "https://telegra.ph/file/cf43d80cd5bbe0fdf3b5a.mp4"
-tmb5 = "https://telegra.ph/file/8c9d1d7c1368631e9178b.mp4"
-tmb6 = "https://telegra.ph/file/f67f3c36091543134bfe3.mp4"
-tmb7 = "https://telegra.ph/file/1199679fb6b90ff012b7a.mp4"
-
-#━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━#
 
 
 plugin_category = "extra"
@@ -29,13 +18,13 @@ plugin_category = "extra"
 async def tmgif(event):
     if event.fwd_from:
         return
-    RR9R7 = await reply_id(event)
-    if tmb:
+    ROZ = await reply_id(event)
+    if cute:
         jmthon = f"**{RR9R7}**\n"
         jmthon += f"✛━━━━━━━━━━━━━✛\n"
         jmthon += f"**الـمتحـرڪـة الأولـى **\n"
         jmthon += f"✛━━━━━━━━━━━━━✛"
-        await event.client.send_file(event.chat_id, tmb, caption=jmthon, reply_to=RR9R7)
+        await event.client.send_file(event.chat_id, cute, caption=jmthon, reply_to=ROZ)
 
 #edit  ~ @RR9R7 for Jmthon 
 #جميع الحقوق محفوظة لسـورس جـمثون تخـمط تبيـن فشلـك
@@ -48,12 +37,11 @@ async def tmgif(event):
     if event.fwd_from:
         return
     leo = await reply_id(event)
-    if tmb2:
+    if cute2:
         RAZAN = f"**{RR9R7}**\n"
         RAZAN += f"✛━━━━━━━━━━━━━✛\n"
         RAZAN += f"**الـمتحـرڪـة الـثـانيـة **\n"
-        RAZAN += f"✛━━━━━━━━━━━━━✛"
-        await event.client.send_file(event.chat_id, tmb2, caption=RAZAN, reply_to=leo)
+        await event.client.send_file(event.chat_id, cute2, caption=RAZAN, reply_to=leo)
 
 #edit  ~ @RR9R7 for Jmthon 
 #جميع الحقوق محفوظة لسـورس جـمثون تخـمط تبيـن فشلـك
@@ -66,12 +54,11 @@ async def tmgif(event):
     if event.fwd_from:
         return
     sic_id = await reply_id(event)
-    if tmb3:
+    if cute3:
         RAZAN = f"**{RR9R7}**\n"
         RAZAN += f"✛━━━━━━━━━━━━━✛\n"
         RAZAN += f"**الـمتحـرڪـة الـثـالثـة**\n"
-        RAZAN += f"✛━━━━━━━━━━━━━✛"
-        await event.client.send_file(event.chat_id, tmb3, caption=RAZAN, reply_to=sic_id)
+        await event.client.send_file(event.chat_id, cute3, caption=RAZAN, reply_to=sic_id)
 
 #edit  ~ @RR9R7 for Jmthon 
 #جميع الحقوق محفوظة لسـورس جـمثون تخـمط تبيـن فشلـك
@@ -84,13 +71,12 @@ async def tmgif(event):
     if event.fwd_from:
         return
     reply_to_id = await reply_id(event)
-    if tmb4:
+    if cute4:
         RAZAN = f"**{RR9R7}**\n"
         RAZAN += f"✛━━━━━━━━━━━━━✛\n"
         RAZAN += f"**الـمتحـرڪـة الـرابـعـة**\n"
-        RAZAN += f"✛━━━━━━━━━━━━━✛"
         await event.client.send_file(
-            event.chat_id, tmb4, caption=RAZAN, reply_to=reply_to_id
+            event.chat_id, cute4, caption=RAZAN, reply_to=reply_to_id
         )
 
 #edit  ~ @RR9R7 for Jmthon 
@@ -105,13 +91,12 @@ async def tmgif(event):
     if event.fwd_from:
         return
     reply_to_id = await reply_id(event)
-    if tmb5:
+    if cute5:
         RAZAN = f"**{RR9R7}**\n"
         RAZAN += f"✛━━━━━━━━━━━━━✛\n"
         RAZAN += f"**الـمتحـرڪـة الـخامسـة**\n"
-        RAZAN += f"✛━━━━━━━━━━━━━✛"
         await event.client.send_file(
-            event.chat_id, tmb5, caption=RAZAN, reply_to=reply_to_id
+            event.chat_id, cute5, caption=RAZAN, reply_to=reply_to_id
         )
 
 #edit  ~ @RR9R7 for Jmthon 
@@ -126,13 +111,12 @@ async def tmgif(event):
     if event.fwd_from:
         return
     reply_to_id = await reply_id(event)
-    if tmb6:
+    if cute6:
         RAZAN = f"**{RR9R7}**\n"
         RAZAN += f"✛━━━━━━━━━━━━━✛\n"
         RAZAN += f"**الـمتحـرڪـة الـسادسـة**\n"
-        RAZAN += f"✛━━━━━━━━━━━━━✛"
         await event.client.send_file(
-            event.chat_id, tmb6, caption=RAZAN, reply_to=reply_to_id
+            event.chat_id, cute6, caption=RAZAN, reply_to=reply_to_id
         )
 
 #edit  ~ @RR9R7 for Jmthon 
@@ -146,13 +130,12 @@ async def tmgif(event):
     if event.fwd_from:
         return
     reply_to_id = await reply_id(event)
-    if tmb7:
+    if cute7:
         RAZAN = f"**{RR9R7}**\n"
         RAZAN += f"✛━━━━━━━━━━━━━✛\n"
-        RAZAN += f"**الـمتحـرڪـة الـسـابعـة**\n"
-        RAZAN += f"✛━━━━━━━━━━━━━✛"
+        RAZAN += f"**الـمتحـرڪـة الـسـابعـة**"
         await event.client.send_file(
-            event.chat_id, tmb7, caption=RAZAN, reply_to=reply_to_id
+            event.chat_id, cute7, caption=RAZAN, reply_to=reply_to_id
         )
     
        
