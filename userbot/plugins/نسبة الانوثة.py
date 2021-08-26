c856535e19f235bde03e60a2a665e26302e1a7c2
- Muhammad ALZUHAIRY <87212030+RR9R7@users.noreply.github.com>
2021-08-26 16:47:37 +0300

    Update نسبة الانوثة.py

--- userbot/plugins/نسبة الانوثة.py
+++ userbot/plugins/نسبة الانوثة.py
@@ -7,7 +7,7 @@ from telethon.tl.functions.photos import GetUserPhotosRequest
 from telethon.tl.functions.users import GetFullUserRequest
 from telethon.utils import get_input_location
 
-from userbot import jmthon, LEO, RAZAN, JASEM
+from userbot import jmthon
 
 from ..Config import Config
 from Jmthon.razan._fun import *
