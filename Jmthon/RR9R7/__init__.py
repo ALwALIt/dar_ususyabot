c8d20d7a4ccf13094542d57a594ff88ee51b847d
- Muhammad ALZUHAIRY <87212030+RR9R7@users.noreply.github.com>
2021-09-03 11:09:26 +0300

    Update __init__.py

--- Jmthon/RR9R7/__init__.py
+++ Jmthon/RR9R7/__init__.py
@@ -3,12 +3,7 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
-
-# the secret configuration specific things
-from ..Config import Config
-from ..core.logger import logging
-
-LOGS = logging.getLogger(__name__)
+from userbot import Config
 
 
 def start() -> scoped_session:
@@ -23,7 +18,7 @@ try:
     SESSION = start()
 except AttributeError as e:
     # this is a dirty way for the work-around required for #23
-    LOGS.error(
+    print(
         "DB_URI is not configured. Features depending on the database might have issues."
     )
-    LOGS.error(str(e))
+    print(str(e))
