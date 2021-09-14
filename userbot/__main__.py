852b92f9c49c1fa83552d5942a9dcfee2d8b357c
- Muhammad ALZUHAIRY <87212030+RR9R7@users.noreply.github.com>
2021-09-14 13:38:13 +0300

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -51,7 +51,7 @@ async def startup_process():
     print("⌯︙بـوت جـيبثون يعـمل بـنجاح ")
     print(
         f"يجـب تفـعيل وضع الأنلايـن ثم أرسـل {cmdhr}فحص لـرؤيـة اذا كـان البوت شـغال\
-        \nللمسـاعدة تواصـل  https://t.me/jmthon"
+        \nللمسـاعدة تواصـل  https://t.me/JEEPTHON"
     )
     print("➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖")
     await verifyLoggerGroup()
