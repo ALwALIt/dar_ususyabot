d3f34581d4fe4dd84b60008a1b098fe9d9850602
almul8ab <91113247+almul8ab@users.noreply.github.com>
2021-10-10 18:33:03 +0300

    Update الصورة الذاتية.py

--- userbot/plugins/الصورة الذاتية.py
+++ userbot/plugins/الصورة الذاتية.py
@@ -1,7 +1,7 @@
 from . import *
 from userbot import jmthon
 
-@jmthon.on(admin_cmd(pattern="جلب الصورة"))
+@jmthon.on(admin_cmd(pattern="ذاتية"))
 async def oho(event):
   if not event.is_reply:
     return await event.edit('يجـب عـليك الـرد عـلى صـورة ذاتيـة الـتدمير')
@@ -9,8 +9,8 @@ async def oho(event):
   pic = await rr9r7.download_media()
   await bot.send_file('me', pic, caption=f"""
 -تـم جـلب الصـورة بنجـاح ✅
-- CH: @Jmthon
-- Dev: @RR9R7
+- CH: @Jepthon
+- Dev: @lMl10l
   """)
   await event.delete()
 
