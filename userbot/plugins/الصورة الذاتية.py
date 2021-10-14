1b5327edc2fadd2bbf744e18546ac68b34b16a69
almul8ab <91113247+almul8ab@users.noreply.github.com>
2021-10-14 03:21:01 +0300

    Create الصورة الذاتية.py

---
+++ userbot/plugins/الصورة الذاتية.py
@@ -0,0 +1,19 @@
+from . import *
+from userbot import jmthon
+
+@jmthon.on(admin_cmd(pattern="ذاتية"))
+async def oho(event):
+  if not event.is_reply:
+    return await event.edit('يجـب عـليك الـرد عـلى صـورة ذاتيـة الـتدمير')
+  rr9r7 = await event.get_reply_message()
+  pic = await rr9r7.download_media()
+  await bot.send_file('me', pic, caption=f"""
+-تـم جلب الصوره
+- CH: @Jepthon
+- Dev:¹: @lMl10l
+  """)
+  await event.delete()
+
+#اخمط وسمي روحك مطور ما اوصيك
+# كتابة محمد الزهيري  
+#@RR7PP
