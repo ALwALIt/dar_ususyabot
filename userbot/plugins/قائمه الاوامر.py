c8d20fafc9ef555fa2f5e07aa107b3011f95523b
almul8ab <91113247+almul8ab@users.noreply.github.com>
2021-10-15 09:26:26 +0300

    Update قائمه الاوامر.py

--- userbot/plugins/قائمه الاوامر.py
+++ userbot/plugins/قائمه الاوامر.py
@@ -110,7 +110,7 @@ async def _(event):
 async def _(event):
 	if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
 		await event.edit(
-		"قائمة اوامر الفارات والتخصيص :\n هـنـا  : \n\n@FYFYFF"
+		"قائمة اوامر الفارات والتخصيص :\n هـنـا  : \n\n@Jepthon2"
 		)
 
 @jmthon.ar_cmd(
