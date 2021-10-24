671567d0eab3f377b315bf2b6207c38b95a3b4e5
Jepthon Arabic <91113247+almul8ab@users.noreply.github.com>
2021-10-24 12:43:55 +0300

    Update بلاي.py

--- userbot/plugins/بلاي.py
+++ userbot/plugins/بلاي.py
@@ -1,6 +1,6 @@
-from . import R
+from . import jmthon
 
-@icssbot.on(icss_cmd(pattern="بلاي$"))
-@icssbot.on(sudo_cmd(pattern="بلاي$", allow_sudo=True))
+@jmthon.on(icss_cmd(pattern="بلاي$"))
+@jmthon.on(sudo_cmd(pattern="بلاي$", allow_sudo=True))
 async def icsrepo(icsp):
     await eor(icsp, R)
