dd352fdae4920b9e4830dc2bfbb1d48635d3d3df
USERBOTJEPTHON <93378583+USERBOTJEPTHON@users.noreply.github.com>
2021-10-29 14:14:45 +0300

    Update and rename userbot/plugins/Tiktok.py to مستخدم الروبوت/الإضافات/Tiktok.py

--- مستخدم الروبوت/الإضافات/Tiktok.py
+++ مستخدم الروبوت/الإضافات/Tiktok.py
@@ -12,18 +12,18 @@ async def _(event):
     if event.fwd_from:
         return 
     if not event.reply_to_msg_id:
-       await event.edit("⌯︙يـجب. الرد علـى الرابط )")
+       await event.edit("⌁︙يـجب. الرد علـى الرابط )")
        return
     reply_message = await event.get_reply_message() 
     if not reply_message.text:
-       await event.edit("⌯︙يـجب. الرد علـى الرابط )")
+       await event.edit("⌁︙يـجب. الرد علـى الرابط )")
        return
     chat = "@TIKTOKDOWNLOADROBOT"
     sender = reply_message.sender
     if reply_message.sender.bot:
-       await event.edit("⌯︙يـجب. الرد علـى الرابط )")
+       await event.edit("⌁︙يـجب. الرد علـى الرابط )")
        return
-    await event.edit("⌯︙جار تحميل الفيديو")
+    await event.edit("⌁︙جار تحميل الفيديو")
     async with event.client.conversation(chat) as conv:
           try:     
               response = conv.wait_event(events.NewMessage(incoming=True,from_users= 1598492699))
@@ -33,7 +33,7 @@ async def _(event):
               await event.reply("```Please unblock me (@QuotLyBot) u Nigga```")
               return
           if response.text.startswith("Hi!"):
-             await event.edit("⌯︙يجـب الغاء خصـوصية التوجيـه اولا")
+             await event.edit("⌁︙يجـب الغاء خصـوصية التوجيـه اولا")
           else: 
              await event.delete()
              await event.client.send_message(event.chat_id, response.message)
