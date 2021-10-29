2e4058f39ecc9a44139f100da5f3fd0017f56f7e
USERBOTJEPTHON <93378583+USERBOTJEPTHON@users.noreply.github.com>
2021-10-29 21:04:24 +0300

    Update التنصيب.py

--- userbot/plugins/التنصيب.py
+++ userbot/plugins/التنصيب.py
@@ -6,4 +6,4 @@ import asyncio
 @borg.on(admin_cmd("تنصيب السورس"))
 async def _(event):
      if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
-        await event.edit("⌯︙**اهـلا بك فـي تنصيب السورس** \n⌯︙رابط التنصيب - [اضغط هنا](https://github.com/USERBOTJEPTHON/JEP-THON/)\n⌯︙قناة السورس - @JepThon")
+        await event.edit("⌁︙**اهـلا بك فـي تنصيب السورس** \n⌁︙رابط التنصيب  -[اضغط هنا](https://github.com/USERBOTJEPTHON/JEP-THON/)\n⌁︙قناة السورس - @JepThon")
