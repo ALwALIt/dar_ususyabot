956f67b4b1b3461cb2924eefe6b84f8691fe7658
USERBOTJEPTHON <93378583+USERBOTJEPTHON@users.noreply.github.com>
2021-10-29 14:24:09 +0300

    Update التنصيب.py

--- userbot/plugins/التنصيب.py
+++ userbot/plugins/التنصيب.py
@@ -3,7 +3,7 @@ import random, re
 from userbot.utils import admin_cmd
 import asyncio 
 
-@borg.on(admin_cmd("التنصيب"))
+@borg.on(admin_cmd("تنصيب السورس"))
 async def _(event):
      if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
-        await event.edit("⌯︙**مـرحبا بـك عزيـزي** \n⌯︙رابط التنصيب - [اضغط هنا](https://dashboard.heroku.com/new?template=https://github.com/JepThon-AR/JepThon-PACK/)\n⌯︙قناة السورس - @JepThon")
+        await event.edit("⌯︙**اهـلا بك فـي تنصيب السورس** \n⌯︙رابط التنصيب - [اضغط هنا](https://github.com/USERBOTJEPTHON/JEP-THON/)\n⌯︙قناة السورس - @JepThon")
