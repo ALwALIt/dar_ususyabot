1d719b90f1cc083581072e55f14b44d25efbee79
USERBOTJEPTHON <93378583+USERBOTJEPTHON@users.noreply.github.com>
2021-10-30 14:00:33 +0300

    Update Vc.py

--- userbot/plugins/Vc.py
+++ userbot/plugins/Vc.py
@@ -37,11 +37,3 @@ async def _(e):
         await edit_or_reply(e, "`جار بدء المكالمة ✅...`")
     except Exception as ex:
         await edit_or_reply(e, f"`{str(ex)}`")
-@jmthon.on(admin_cmd(pattern="غلق المكالمه(?: |$)(.*)"))
-async def _(e):
-    try:
-        await e.client(DiscardGroupCallRequest(await stopvc(e)))
-        await edit_or_reply(e, "`جار انهاء المكالمة ✅...`")
-    except Exception as ex:
-        await edit_or_reply(e, f"`{str(ex)}`")
-
