521871f20efc8cdd967c5fc73f98b64de12dca9e
USERBOTJEPTHON <93378583+USERBOTJEPTHON@users.noreply.github.com>
2021-10-30 12:50:19 +0300

    Create Vc.py

---
+++ userbot/plugins/Vc.py
@@ -0,0 +1,39 @@
+from telethon.tl.functions.phone import CreateGroupCallRequest as startvc
+from telethon.tl.functions.phone import DiscardGroupCallRequest as stopvc
+from telethon.tl.functions.phone import GetGroupCallRequest as getvc
+from telethon.tl.functions.phone import InviteToGroupCallRequest as invitetovc
+from userbot import jmthon
+from ..core.managers import edit_delete, edit_or_reply
+
+async def get_call(event):
+    mm = await event.client(getchat(event.chat_id))
+    xx = await event.client(getvc(mm.full_chat.call))
+    return xx.call
+
+def user_list(l, n):
+    for i in range(0, len(l), n):
+        yield l[i : i + n]
+
+@jmthon.on(admin_cmd(pattern="دعوه للمكالمه(?: |$)(.*)"))
+async def _(e):
+    ok = await eor(e, "`Inviting Members to Voice Chat...`")
+    users = []
+    z = 0
+    async for x in e.client.iter_participants(e.chat_id):
+        if not x.bot:
+            users.append(x.id)
+    hmm = list(user_list(users, 6))
+    for p in hmm:
+        try:
+            await e.client(invitetovc(call=await get_call(e), users=p))
+            z += 6
+        except BaseException:
+            pass
+    await ok.edit(f"`Invited {z} users`")
+@jmthon.on(admin_cmd(pattern="بدء مكالمه(?: |$)(.*)"))
+async def _(e):
+    try:
+        await e.client(startvc(e.chat_id))
+        await eor(e, "`Voice Chat Started...`")
+    except Exception as ex:
+        await eor(e, f"`{str(ex)}`")
