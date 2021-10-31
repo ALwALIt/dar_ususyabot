3197a244b9cfdb570a9641f92ad4c2b96f74bfbf
USERBOTJEPTHON <93378583+USERBOTJEPTHON@users.noreply.github.com>
2021-10-31 08:18:57 +0300

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -8,8 +8,6 @@ from .core.logger import logging
 from .core.session import jmthon
 from .utils import (
     add_bot_to_logger_group,
-    autojo,
-    autozs,
     ipchange,
     load_plugins,
     setup_bot,
@@ -17,7 +15,7 @@ from .utils import (
     verifyLoggerGroup,
 )
 
-LOGS = logging.getLogger("Jepthon")
+LOGS = logging.getLogger("JepThon")
 
 print(userbot.__copyright__)
 print("Licensed under the terms of the " + userbot.__license__)
@@ -50,10 +48,10 @@ async def startup_process():
     await load_plugins("plugins")
     await load_plugins("assistant")
     print("➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖")
-    print("⌯︙بـوت جيبثون يعـمل بـنجاح ")
+    print("⌯︙بـوت جـيبثون يعـمل بـنجاح ")
     print(
         f"يجـب تفـعيل وضع الأنلايـن ثم أرسـل {cmdhr}فحص لـرؤيـة اذا كـان البوت شـغال\
-        \nللمسـاعدة تواصـل  https://t.me/Jepthon"
+        \nللمسـاعدة تواصـل  https://t.me/JepThon"
     )
     print("➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖")
     await verifyLoggerGroup()
@@ -65,10 +63,8 @@ async def startup_process():
     return
 
 
-jmthon.loop.run_until_complete(startup_process())
-jmthon.loop.run_until_complete(autozs())
-jmthon.loop.run_until_complete(autojo())
 
+jmthon.loop.run_until_complete(startup_process())
 
 if len(sys.argv) not in (1, 3, 4):
     jmthon.disconnect()
