3635dee4b0bcdf4424c7d99da932b09601261bb7
USERBOTJEPTHON <93378583+USERBOTJEPTHON@users.noreply.github.com>
2021-11-01 15:06:49 +0300

    Update كشف.py

--- userbot/plugins/كشف.py
+++ userbot/plugins/كشف.py
@@ -141,7 +141,7 @@ async def _(event):
 
 
 @jmthon.ar_cmd(
-    pattern="ايدي(?:\s|$)([\s\S]*)" or "ا(?:\s|$)([\s\S]*)",
+    pattern="ايدي(?:\s|$)([\s\S]*)",
     command=("ايدي", plugin_category) or ("ا", plugin_category),
     info={
         "header": "Gets info of an user.",
