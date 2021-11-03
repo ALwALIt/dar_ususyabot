4e8892bdfe26a8f6e8b3e8e209ed82ef0451da3f
USERBOTJEPTHON <93378583+USERBOTJEPTHON@users.noreply.github.com>
2021-11-03 16:15:29 +0300

    Update يوتيوب.py

--- userbot/plugins/يوتيوب.py
+++ userbot/plugins/يوتيوب.py
@@ -374,16 +374,21 @@ async def kakashi(event):
     await event.client.delete_messages(
         conv.chat_id, [msg_start.id, response.id, msg.id, video.id, details.id]
     )
-@jmthon.on(admin_cmd(pattern="معنى(?: |$)(.*)"))
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+
+from userbot import jmthon
+
+
+@jmthon.on(admin_cmd(pattern="تيك توك(?: |$)(.*)"))
 async def _(event):
     if event.fwd_from:
         return
     r_link = event.pattern_match.group(1)
     if ".com" not in r_link:
-        await event.edit("**▾∮ اكتب الاسم اثول **")
+        await event.edit("**▾∮ يجب وضع رابط الفيديو مع الامر اولا **")
     else:
         await event.edit("**▾∮ تتم المعالجة انتظر قليلا**")
-    chat = "@EX6bot"
+    chat = "@ttsavebot"
     async with bot.conversation(chat) as conv:
         try:
             msg_start = await conv.send_message("/start")
@@ -391,10 +396,10 @@ async def _(event):
             msg = await conv.send_message(r_link)
             details = await conv.get_response()
             video = await conv.get_response()
-            """ كتابة فريق جيبثون  @Jepthon """
+            """ قناة جيبثون  @Jepthon """
             await bot.send_read_acknowledge(conv.chat_id)
         except YouBlockedUserError:
-            await event.edit("▾∮ الغـي حـظر هـذا البـوت و حـاول مجـددا @EX6bot")
+            await event.edit("▾∮ الغـي حـظر هـذا البـوت و حـاول مجـددا @ttsavebot")
             return
         await bot.send_file(event.chat_id, video)
         await event.client.delete_messages(
