7aa4107a72f274cb60b4c6de0eee82b6680b5678
USERBOTJEPTHON <93378583+USERBOTJEPTHON@users.noreply.github.com>
2021-11-03 15:52:33 +0300

    Update يوتيوب.py

--- userbot/plugins/يوتيوب.py
+++ userbot/plugins/يوتيوب.py
@@ -9,6 +9,7 @@ import re
 import time
 from datetime import datetime
 
+from userbot.utils import sudo_cmd
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 from telethon.tl import types
 from telethon.utils import get_attributes
@@ -373,3 +374,39 @@ async def kakashi(event):
     await event.client.delete_messages(
         conv.chat_id, [msg_start.id, response.id, msg.id, video.id, details.id]
     )
+@jmthon.on(admin_cmd(pattern="معنى  ?(.*)"))
+@jmthon.on(sudo_cmd(pattern="معنى ?(.*)", allow_sudo=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    input_str = event.pattern_match.group(1)
+    reply_to_id = await reply_id(event)
+    if event.reply_to_msg_id and not event.pattern_match.group(1):
+        reply_to_id = await event.get_reply_message()
+        reply_to_id = str(reply_to_id.message)
+    else:
+        reply_to_id = str(event.pattern_match.group(1))
+    if not reply_to_id:
+        return await edit_or_reply(
+            event, "**  .معنى + الاسم ...  \n مثـال**   :  `.معنى محدد`"
+        )
+    chat = "@EX6bot"
+    catevent = await edit_or_reply(event, "**- جـارِ عـرض المعـلومات انتـظر 🗿🔥 ⌁︙**")
+    async with event.client.conversation(chat) as conv:
+        try:
+            response = conv.wait_event(
+                events.NewMessage(incoming=True, from_users=1188524603)
+            )
+            await event.client.send_message(chat, "{}".format(input_str))
+            response = await response
+            await event.client.send_read_acknowledge(conv.chat_id)
+        except YouBlockedUserError:
+            await catevent.edit(
+                "**- تحـقق من انـك لم تقـم بحظر البوت @EX6bot .. ثم اعـد استخدام الامـر 🔥⌁︙**"
+            )
+            return
+        if response.text.startswith("I can't find that"):
+            await catevent.edit("**- عـذراً .. لـم استطـع ايجـاد المطلـوب ⌁︙**")
+        else:
+            await catevent.delete()
+            await event.client.send_message(event.chat_id, response.message)
