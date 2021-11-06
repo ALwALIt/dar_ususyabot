5a2adef1952f0442bd008898d1e52d2e952ed41c
USERBOTJEPTHON <93378583+USERBOTJEPTHON@users.noreply.github.com>
2021-11-06 08:54:12 +0300

    Update بوتات.py

--- userbot/plugins/بوتات.py
+++ userbot/plugins/بوتات.py
@@ -150,3 +150,37 @@ async def _(event):
             await catevent.delete()
             await event.client.send_message(event.chat_id, response.message)
 
+@bot.on(admin_cmd(pattern="برج ?(.*)"))
+async def _(event):
+    if event.reply_to_msg_id:
+        return
+    input_str = event.pattern_match.group(1)
+    reply_to_id = await reply_id(event)
+    if event.reply_to_msg_id and not event.pattern_match.group(1):
+        reply_to_id = await event.get_reply_message()
+        reply_to_id = str(reply_to_id.message)
+    else:
+        reply_to_id = str(event.pattern_match.group(1))
+    if not reply_to_id:
+        return await edit_or_reply(
+            event, "**╮ .برج + اسم برجك ... ...╰**"
+        )
+    chat = "@TermexJepBoT"
+    catevent = await edit_or_reply(event, "**╮•⎚ اصبر جاي نطلع برجك ... 🧸🎈**")
+    async with event.client.conversation(chat) as conv:
+        try:
+            response = conv.wait_event(
+                events.NewMessage(incoming=True, from_users=2142702957)
+            )
+            await event.client.send_message(chat, "{}".format(input_str))
+            response = await response
+            await event.client.send_read_acknowledge(conv.chat_id)
+        except YouBlockedUserError:
+            await catevent.edit("**╮•⎚ تحـقق من انـك لم تقـم بحظر البوت @TermexJepBoT .. ثم اعـد استخدام الامـر ...🤖♥️**")
+            return
+        if response.text.startswith("I can't find that"):
+            await catevent.edit("**╮•⎚ عـذراً .. لـم استطـع ايجـاد المطلـوب ☹️💔**")
+        else:
+            await catevent.delete()
+            await event.client.send_message(event.chat_id, response.message)
+
