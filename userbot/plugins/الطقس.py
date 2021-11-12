430b70d8f92694cb641f74ddfed9a1cd753fbe86
USERBOTJEPTHON <93378583+USERBOTJEPTHON@users.noreply.github.com>
2021-11-12 14:03:26 +0300

    Create الطقس.py

---
+++ userbot/plugins/الطقس.py
@@ -0,0 +1,70 @@
+import io
+import time
+
+import aiohttp
+
+from jmthon.utils import admin_cmd
+
+
+@jmthon.on(admin_cmd(pattern="طقس (.*)"))
+@jmthon.on(sudo_cmd(pattern="طقس (.*)", allow_sudo=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    sample_url = (
+        "https://api.openweathermap.org/data/2.5/weather?q={}&APPID={}&units=metric"
+    )
+    input_str = event.pattern_match.group(1)
+    async with aiohttp.ClientSession() as session:
+        response_api_zero = await session.get(
+            sample_url.format(input_str, Config.OPEN_WEATHER_MAP_APPID)
+        )
+    response_api = await response_api_zero.json()
+    if response_api["cod"] == 200:
+        country_code = response_api["sys"]["country"]
+        country_time_zone = int(response_api["timezone"])
+        sun_rise_time = int(response_api["sys"]["sunrise"]) + country_time_zone
+        sun_set_time = int(response_api["sys"]["sunset"]) + country_time_zone
+        await eor(
+            event,
+            """{}
+**درجة الحرارة**: {}°С
+    __الحد الادنى__: {}°С
+    __الحد الاقصى__ : {}°С
+**الرطوبة**: {}%
+**الرياح**: {}m/s
+**السحاب**: {}hpa
+**شروق الشمس**: {} {}
+**الشمس**: {} {}""".format(
+                input_str,
+                response_api["main"]["temp"],
+                response_api["main"]["temp_min"],
+                response_api["main"]["temp_max"],
+                response_api["main"]["humidity"],
+                response_api["wind"]["speed"],
+                response_api["clouds"]["all"],
+                # response_api["main"]["pressure"],
+                time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime(sun_rise_time)),
+                country_code,
+                time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime(sun_set_time)),
+                country_code,
+            ),
+        )
+    else:
+        await eor(event, response_api["message"])
+
+
+@jmthon.on(admin_cmd(pattern="wttr (.*)"))
+async def _(event):
+    if event.fwd_from:
+        return
+    sample_url = "https://wttr.in/{}.png"
+    # logger.info(sample_url)
+    input_str = event.pattern_match.group(1)
+    async with aiohttp.ClientSession() as session:
+        response_api_zero = await session.get(sample_url.format(input_str))
+        # logger.info(response_api_zero)
+        response_api = await response_api_zero.read()
+        with io.BytesIO(response_api) as out_file:
+            await event.reply(file=out_file)
+    await eor(event, input_str)
