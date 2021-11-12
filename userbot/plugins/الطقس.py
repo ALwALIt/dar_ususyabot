29d723569fb8539eba976b36f3703820ea294c27
USERBOTJEPTHON <93378583+USERBOTJEPTHON@users.noreply.github.com>
2021-11-12 15:23:48 +0300

    Update الطقس.py

--- userbot/plugins/الطقس.py
+++ userbot/plugins/الطقس.py
@@ -59,7 +59,7 @@ async def _(event):
         await edit_or_reply(event, response_api["message"])
 
 
-@jmthon.on(admin_cmd(pattern="wttr (.*)"))
+@jmthon.on(admin_cmd(pattern="طقس (.*)"))
 async def _(event):
     if event.fwd_from:
         return
