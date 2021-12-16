043ea20fe822a6a46ad10c1e05b96fa878b3b06e
USERBOTJEPTHON <93378583+USERBOTJEPTHON@users.noreply.github.com>
2021-12-17 00:43:52 +0300

    Update iytdl.py

--- userbot/assistant/iytdl.py
+++ userbot/assistant/iytdl.py
@@ -45,8 +45,8 @@ plugin_category = "bot"
 
 
 @jmthon.ar_cmd(
-    pattern="بحث(?:\s|$)([\s\S]*)",
-    command=("بحث", plugin_category),
+    pattern="يوتيوب(?:\s|$)([\s\S]*)",
+    command=("يوتيوب", plugin_category),
     info={
         "header": "ytdl with inline buttons.",
         "description": "To search and download youtube videos by inline buttons.",
