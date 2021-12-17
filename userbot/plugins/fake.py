15fd661b6ffe0fb93d3dfcc553b8a94a81d9db4f
USERBOTJEPTHON <93378583+USERBOTJEPTHON@users.noreply.github.com>
2021-12-18 01:55:18 +0300

    Update fake.py

--- userbot/plugins/fake.py
+++ userbot/plugins/fake.py
@@ -1,125 +1,262 @@
 import asyncio
-from random import choice, randint
+import time
 
-from telethon.errors import BadRequestError
-from telethon.tl.functions.channels import EditAdminRequest
-from telethon.tl.types import ChatAdminRights
+import aiohttp
+from telethon.errors import ChatAdminRequiredError as no_admin
+from telethon.tl.functions.messages import ExportChatInviteRequest
+
+from Jmthon.razan.resources.strings import *
+from userbot import jmthon
+from userbot.utils import admin_cmd
+
+from ..core.managers import edit_or_reply
+from ..core.managers import edit_or_reply as eod
+from ..helpers import get_user_from_event
+from . import *
+
+
+@jmthon.on(admin_cmd(pattern="كتابة(?: |$)(.*)"))
+async def _(event):
+    t = event.pattern_match.group(1)
+    if not (t or t.isdigit()):
+        t = 100
+    else:
+        try:
+            t = int(t)
+        except BaseException:
+            try:
+                t = await event.ban_time(t)
+            except BaseException:
+                return await event.edit("**- يجب كتابة الامر بشكل صحيح**")
+    await event.edit(f"**تم بدء وضع الكتابة الوهمية لـ {t} من الثوانـي**")
+    async with event.client.action(event.chat_id, "typing"):
+        await asyncio.sleep(t)
+
+
+@jmthon.on(admin_cmd(pattern="صوتية(?: |$)(.*)"))
+async def _(event):
+    t = event.pattern_match.group(1)
+    if not (t or t.isdigit()):
+        t = 100
+    else:
+        try:
+            t = int(t)
+        except BaseException:
+            try:
+                t = await event.ban_time(t)
+            except BaseException:
+                return await event.edit("**- يجب كتابة الامر بشكل صحيح**")
+    await event.edit(f"**تم بدء وضع ارسال الصوتية الوهمية لـ {t} من الثوانـي**")
+    async with event.client.action(event.chat_id, "record-audio"):
+        await asyncio.sleep(t)
+
+
+@jmthon.on(admin_cmd(pattern="فيد(?: |$)(.*)"))
+async def _(event):
+    t = event.pattern_match.group(1)
+    if not (t or t.isdigit()):
+        t = 100
+    else:
+        try:
+            t = int(t)
+        except BaseException:
+            try:
+                t = await event.ban_time(t)
+            except BaseException:
+                return await event.edit("**- يجب كتابة الامر بشكل صحيح**")
+    await event.edit(f"**تم بدء وضع ارسال الفيديو الوهمي لـ {t} من الثوانـي**")
+    async with event.client.action(event.chat_id, "record-video"):
+        await asyncio.sleep(t)
+
+
+@jmthon.on(admin_cmd(pattern="لعبة(?: |$)(.*)"))
+async def _(event):
+    t = event.pattern_match.group(1)
+    if not (t or t.isdigit()):
+        t = 100
+    else:
+        try:
+            t = int(t)
+        except BaseException:
+            try:
+                t = await event.ban_time(t)
+            except BaseException:
+                return await event.edit("**- يجب كتابة الامر بشكل صحيح**")
+    await event.edit(f"**تم بدء وضع اللعب الوهمي لـ {t} من الثوانـي**")
+    async with event.client.action(event.chat_id, "game"):
+        await asyncio.sleep(t)
+
+
+@jmthon.on(admin_cmd(pattern="الرابط$"))
+async def _(e):
+    rr = await edit_or_reply(e, "**يتم جلب الرابط انتظر **")
+    try:
+        r = await e.client(
+            ExportChatInviteRequest(e.chat_id),
+        )
+    except no_admin:
+        return await eod(rr, "عذرا انت لست مشرف في هذه الدردشة", time=10)
+    await eod(rr, f"- رابط الدردشة\n {r.link}")
+
+
+@jmthon.on(admin_cmd(pattern="للكل تاك$"))
+async def listall(jmthon):
+    if jmthon.fwd_from:
+        return
+    mentions = "- هذه هي قائمة جميع الاعضاء هنا: "
+    chat = await bot.get_input_chat()
+    async for x in borg.iter_participants(chat, 2000):
+        mentions += f" \n[{x.first_name}](tg://user?id={x.id})"
+    await jmthon.reply(mentions)
+    await jmthon.delete()
+
+
+# don't kang the file your mother puusy
+# code by @jmthon
+
+@jmthon.on(admin_cmd(pattern="طقس (.*)"))
+@jmthon.on(sudo_cmd(pattern="طقس (.*)", allow_sudo=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    Key = "f806cebbd44f34cc4cd1d79a290081be"
+    sample_url = "https://api.openweathermap.org/data/2.5/weather?q={}&appid={}"
+    input_str = event.pattern_match.group(1)
+    async with aiohttp.ClientSession() as session:
+        response_api_zero = await session.get(sample_url.format(input_str, Key))
+    response_api = await response_api_zero.json()
+    if response_api["cod"] == 200:
+        country_code = response_api["sys"]["country"]
+        country_time_zone = int(response_api["timezone"])
+        sun_rise_time = int(response_api["sys"]["sunrise"]) + country_time_zone
+        sun_set_time = int(response_api["sys"]["sunset"]) + country_time_zone
+        await edit_or_reply(
+            event,
+            """{}
+-**الحـرارة**: {}°С
+-**درجة الحرارة الصغرى:** {}°С
+-**درجة الحرارة العظم:** {}°С
+**الرطـوبة**: {}%
+-**الـرياح**: {}m/s
+-**السحـاب**: {}hpa
+-**شروق الشمس**: {} {}
+-**غروب الشمس**: {} {}""".format(
+                input_str,
+                response_api["main"]["temp"],
+                response_api["main"]["temp_min"],
+                response_api["main"]["temp_max"],
+                response_api["main"]["humidity"],
+                response_api["wind"]["speed"],
+                response_api["clouds"]["all"],
+                # response_api["main"]["pressure"],
+                time.strftime("%Y-%m-%d %I:%M:%S", time.gmtime(sun_rise_time)),
+                country_code,
+                time.strftime("%Y-%m-%d %I:%M:%S", time.gmtime(sun_set_time)),
+                country_code,
+            ),
+        )
+    else:
+        await edit_or_reply(event, response_api["message"])
+
+
+import asyncio
+
+from jmthon.utils import admin_cmd
 
 from userbot import jmthon
 
-from ..core.managers import edit_delete, edit_or_reply
-from ..helpers.utils import get_user_from_event
-from . import ALIVE_NAME
-
-plugin_category = "fun"
-
-
-@jmthon.ar_cmd(
-    pattern="فيك(?:\s|$)([\s\S]*)",
-    command=("فيك", plugin_category),
-    info={
-        "header": "To show fake actions for a paticular period of time",
-        "description": "if time is not mentioned then it may choose random time 5 or 6 mintues for mentioning time use in seconds",
-        "usage": [
-            "{tr}فيك <action> <time(in seconds)>",
-            "{tr}فيك <action>",
-            "{tr}scam",
-        ],
-        "examples": "{tr}scam photo 300",
-        "actions": [
-        "typing",
-        "contact",
-        "game",
-        "location",
-        "voice",
-        "round",
-        "video",
-        "photo",
-        "document",
-
-        ],
-    },
+R = (
+    "┈┈╱▔▔▔▔▔▔▔▔▔▔▔▏\n"
+    "┈╱╭▏╮╭┻┻╮╭┻┻╮╭▏ \n"
+    "▕╮╰▏╯┃╭╮┃┃╭╮┃╰▏ \n"
+    "▕╯┈▏┈┗┻┻┛┗┻┻┻╮▏ \n"
+    "▕╭╮▏╮┈┈┈┈┏━━━╯▏\n"
+    "▕╰╯▏╯╰┳┳┳┳┳┳╯╭▏ \n"
+    "▕┈╭▏╭╮┃┗┛┗┛┃┈╰▏ \n"
+    "▕┈╰▏╰╯╰━━━━╯┈┈▏\n"
 )
-async def _(event):
-    options = [
-        "typing",
-        "contact",
-        "game",
-        "location",
-        "voice",
-        "round",
-        "video",
-        "photo",
-        "document",
-
-    ]
-    input_str = event.pattern_match.group(1)
-    args = input_str.split()
-    if len(args) == 0:
-        scam_action = choice(options)
-        scam_time = randint(300, 360)
-    elif len(args) == 1:
-        try:
-            scam_action = str(args[0]).lower()
-            scam_time = randint(300, 360)
-        except ValueError:
-            scam_action = choice(options)
-            scam_time = int(args[0])
-    elif len(args) == 2:
-        try:
-            scam_action = str(args[0]).lower()
-            scam_time = int(args[1])
-        except ValueError:
-            return await edit_delete(event, "`Invalid Syntax !!`")
-    else:
-        return await edit_delete(event, "`Invalid Syntax !!`")
-    try:
-        if scam_time > 0:
-            await event.delete()
-            async with event.client.action(event.chat_id, scam_action):
-                await asyncio.sleep(scam_time)
-    except BaseException:
-        return
 
 
-@jmthon.ar_cmd(
-    pattern="رفع ادمن$",
-    command=("رفع ادمن", plugin_category),
-    info={
-        "header": "Fun animation for faking user promotion",
-        "description": "An animation that shows enabling all permissions to him that he is admin(fake promotion)",
-        "usage": "{tr}padmin",
-    },
-    groups_only=True,
+@jmthon.on(admin_cmd(pattern=r"سبونج"))
+async def kerz(kerz):
+    await kerz.edit(R)
+
+    A = (
+        "╭━┳━╭━╭━╮╮\n"
+        "┃┈┈┈┣▅╋▅┫┃\n"
+        "┃┈┃┈╰━╰━━━━━━╮\n"
+        "╰┳╯┈┈┈┈┈┈┈┈┈◢▉◣\n"
+        "╲┃┈┈┈┈┈┈┈┈┈┈▉▉▉\n"
+        "╲┃┈┈┈┈┈┈┈┈┈┈◥▉◤\n"
+        "╲┃┈┈┈┈╭━┳━━━━╯\n"
+        "╲┣━━━━━━┫\n"
+    )
+
+
+@jmthon.on(admin_cmd(pattern=r"كلب"))
+async def dog(dog):
+    await dog.edit(A)
+    Z = (
+        "┈┏━╮╭━┓┈╭━━━━╮\n"
+        "┈┃┏┗┛┓┃╭┫ⓞⓘⓝⓚ┃\n"
+        "┈╰┓▋▋┏╯╯╰━━━━╯\n"
+        "╭━┻╮╲┗━━━━╮╭╮┈\n"
+        "┃▎▎┃╲╲╲╲╲╲┣━╯┈\n"
+        "╰━┳┻▅╯╲╲╲╲┃┈┈┈\n"
+        "┈┈╰━┳┓┏┳┓┏╯┈┈┈\n"
+        "┈┈┈┈┗┻┛┗┻┛┈┈┈┈\n"
+    )
+
+
+A = (
+    " ╱▏┈┈┈┈┈┈▕╲▕╲┈┈┈\n"
+    "▏▏┈┈┈┈┈┈▕▏▔▔╲┈┈\n"
+    "▏╲┈┈┈┈┈┈╱┈▔┈▔╲┈\n"
+    "╲▏▔▔▔▔▔▔╯╯╰┳━━▀\n"
+    "┈▏╯╯╯╯╯╯╯╯╱┃┈┈┈\n"
+    "┈┃┏┳┳━━━┫┣┳┃┈┈┈\n"
+    "┈┃┃┃┃┈┈┈┃┃┃┃┈┈┈\n"
+    "┈┗┛┗┛┈┈┈┗┛┗┛┈┈┈\n"
 )
-async def _(event):
-    "Fun animation for faking user promotion."
-    animation_interval = 1
-    animation_ttl = range(20)
-    event = await edit_or_reply(event, "`جارِ ترقيتك.......`")
-    animation_chars = [
-        "**تحويل ملكية المجموعة الى المستخدم...**",
-        "**اعطاء كافة الصلاحيات الى المستخدم...**",
-        "**(1) ارسال الرسائل: ☑️**",
-        "**(1) ارسال الرسائل: ✅**",
-        "**(2) ارسال الوسائط: ☑️**",
-        "**(2) ارسال الوسائط: ✅**",
-        "**(3) ارسال الملصقات & المتحركات: ☑️**",
-        "**(3) ارسال الملصقات & المتحركات: ✅**",
-        "**(4) ارسال الاستفتاء: ☑️**",
-        "**(4) ارسال الاستفتاء: ✅**",
-        "**(5) حظر المستخدمين: ☑️**",
-        "**(5) حظر المستخدمين: ✅**",
-        "**(6) اضافة عضو: ☑️**",
-        "**(6) اضافة عضو: ✅**",
-        "**(7) تثبيت الرسائل: ☑️**",
-        "**(7) تثبيت الرسائل: ✅**",
-        "**(8) تغير معلومات المجموعة: ☑️**",
-        "**(8) تغير معلومات المجموعة: ✅**",
-        "**تم ترقيتك بنجاح ✅**",
-        f"**لقد تم رفعك مالك المجموعة من قبل: {ALIVE_NAME}**",
-    ]
-    for i in animation_ttl:
-        await asyncio.sleep(animation_interval)
-        await event.edit(animation_chars[i % 20])
-#Jepthon @lMl10l
+
+A = (
+    "┈┈┈┈╱▔▔▔▔▔╲┈╱▔╲\n"
+    "┈┈┈┈▏┈┈▏╭╮▕┈▏╳▕\n"
+    "┈┈┈┈▏┈┈▏┈┈▕┈╲▂╱\n"
+    "┈╱▔▔╲▂╱╲▂▂┈╲▂▏▏\n"
+    "╭▏┈┈┈┈┈┈┈▏╲▂▂╱┈\n"
+    "┃▏┈┈┈┈▏┈┈▏┈┈┈┈┈\n"
+    "╯▏┈╲╱▔╲▅▅▏┈┈┈┈\n"
+    "┈╲▅▅▏▕▔▔▔▔▏┈┈┈┈\n"
+)
+
+N = (
+    "┈┈┈╭━━━━━╮┈┈┈┈┈\n"
+    "┈┈┈┃┊┊┊┊┊┃┈┈┈┈┈\n"
+    "┈┈┈┃┊┊╭━╮┻╮┈┈┈┈\n"
+    "┈┈┈╱╲┊┃▋┃▋┃┈┈┈┈\n"
+    "┈┈╭┻┊┊╰━┻━╮┈┈┈┈\n"
+    "┈┈╰┳┊╭━━━┳╯┈┈┈┈\n"
+    "┈┈┈┃┊┃╰━━┫┈┈┈┈\n"
+    "┈┈┈┈┈┈┏━┓┈┈┈┈┈┈\n"
+)
+
+
+@jmthon.on(admin_cmd(pattern=r"ذئب"))
+async def fox(fox):
+    await fox.edit(A)
+
+
+@jmthon.on(admin_cmd(pattern=r"فيل"))
+async def elephant(elephant):
+    await elephant.edit(A)
+
+
+@jmthon.on(admin_cmd(pattern=r"هومر"))
+async def homer(homer):
+    await homer.edit(N)
+
+
+@jmthon.on(admin_cmd(pattern=r"بك"))
+async def pig(pig):
+    await pig.edit(Z)
