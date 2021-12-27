1a624161363f1181f52f26dced388f7a7be4ca76
USERBOTJEPTHON <93378583+USERBOTJEPTHON@users.noreply.github.com>
2021-12-27 05:49:25 -0800

    Update voicememe.py

--- userbot/plugins/voicememe.py
+++ userbot/plugins/voicememe.py
@@ -1,12 +1,12 @@
 import re
-from ..helpers.utils import reply_id
+from ..helpers.utils import reply_id as rd
 from userbot import jmthon
 
 @jmthon.on(admin_cmd(outgoing=True, pattern="ص1$"))
-async def jpvois(vois):
+async def iqvois(vois):
     if vois.fwd_from:
         return
-    Ti = await reply_id(vois)
-    if jpvois1:
-        await vois.client.send_file(vois.chat_id, jpvois1, reply_to=Ti)
+    Ti = await rd(vois)
+    if iqvois1:
+        await vois.client.send_file(vois.chat_id, iqvois1, reply_to=Ti)
         await vois.delete()
