354f0e3f1f341272203ea09ec0d9951d3c2fd4c5
USERBOTJEPTHON <93378583+USERBOTJEPTHON@users.noreply.github.com>
2022-01-02 14:27:53 +0300

    Update متحركات ساد.py

--- userbot/plugins/متحركات ساد.py
+++ userbot/plugins/متحركات ساد.py
@@ -167,6 +167,66 @@ async def tmgif(event):
         await event.client.send_file(
             event.chat_id, sad9, caption=RAZAN, reply_to=reply_to_id
         )
-      
+      @catub.cat_cmd(
+    pattern="kiss(?:\s|$)([\s\S]*)",
+    command=("kiss", plugin_category),
+    info={
+        "header": "Sends random kiss",
+        "usage": [
+            "{tr}kiss",
+            "{tr}kiss <1-20>",
+        ],
+    },
+)
+async def some(event):
+    """Its useless for single like you. Get a lover first"""
+    inpt = event.pattern_match.group(1)
+    reply_to_id = await reply_id(event)
+    count = 1 if not inpt else int(inpt)
+    if count < 0 and count > 20:
+        await edit_delete(event, "`Give value in range 1-20`")
+    res = base64.b64decode(
+        "aHR0cHM6Ly90Lm1lL2pvaW5jaGF0L0NtZEEwVzYtSVVsbFpUUTk="
+    ).decode("utf-8")
+    resource = await event.client(GetFullChannelRequest(res))
+    chat = resource.chats[0].username
+    try:
+        await event.client(
+            functions.channels.GetParticipantRequest(
+                channel=chat, participant=event.from_id.user_id
+            )
+        )
+    except UserNotParticipantError:
+        await event.client(Get(res.split("/")[4]))
+        await event.client.edit_folder(resource.full_chat.id, 1)
+        await event.client(
+            functions.account.UpdateNotifySettingsRequest(
+                peer=chat,
+                settings=types.InputPeerNotifySettings(
+                    show_previews=False,
+                    silent=True,
+                ),
+            )
+        )
+    catevent = await edit_or_reply(event, "`Wait babe...`😘")
+    maxmsg = await event.client.get_messages(chat)
+    start = random.randint(31, maxmsg.total)
+    start = min(start, maxmsg.total - 40)
+    end = start + 41
+    kiss = []
+    async for x in event.client.iter_messages(
+        chat, min_id=start, max_id=end, reverse=True
+    ):
+        try:
+            if x.media and x.media.document.mime_type == "video/mp4":
+                link = f"{res.split('j')[0]}{chat}/{x.id}"
+                kiss.append(link)
+        except AttributeError:
+            pass
+    kisss = random.sample(kiss, count)
+    for i in kisss:
+        nood = await event.client.send_file(event.chat_id, i, reply_to=reply_to_id)
+        await _catutils.unsavegif(event, nood)
+    await catevent.delete()
 #edit  ~ @RR9R7 for Jmthon 
 #جميع الحقوق محفوظة لسـورس جـيبثون تخـمط تبيـن فشلـك
