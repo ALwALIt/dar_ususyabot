28898221451f875763c2db69c08bfe90466b84fc
USERBOTJEPTHON <93378583+USERBOTJEPTHON@users.noreply.github.com>
2022-01-18 15:26:07 +0300

    Update الصورة الذاتية.py

--- userbot/plugins/الصورة الذاتية.py
+++ userbot/plugins/الصورة الذاتية.py
@@ -1,18 +1,22 @@
-from . import *
+from userbot import *
 from userbot import jmthon
 
 @jmthon.on(admin_cmd(pattern="(ذاتية|اح)"))
-async def oho(event):
-  if not event.is_reply:
-    return await event.edit('يجـب عـليك الـرد عـلى صـورة ذاتيـة الـتدمير')
-  rr9r7 = await event.get_reply_message()
-  pic = await rr9r7.download_media()
-  await bot.send_file('me', pic, caption=f"""
--تـم جلب الصوره
+async def dato(event):
+    if not event.is_reply:
+        return await event.edit("..")
+    rr9r7 = await event.get_reply_message()
+    pic = await rr9r7.download_media()
+    await bot.send_file(
+        "me",
+        pic,
+        caption=f"""
+-تـم جـلب الصـورة بنجـاح ✅
 - CH: @Jepthon
-- Dev:¹: @lMl10l
-  """)
-  await event.delete(كيوت❤️‍🔥)
+- Dev: @lMl10l
+  """,
+    )
+    await event.edit("كيوت ❤️‍🔥")
 
 #اخمط وسمي روحك مطور ما اوصيك
 # كتابة السيد حسين  
