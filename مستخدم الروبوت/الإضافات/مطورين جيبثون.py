47153c418d0e0b50dc96fbb3916a8806081b09e7
USERBOTJEPTHON <93378583+USERBOTJEPTHON@users.noreply.github.com>
2022-01-20 20:37:31 +0300

    Update and rename userbot/plugins/مطورين جيبثون.py to مستخدم الروبوت/الإضافات/مطورين جيبثون.py

--- مستخدم الروبوت/الإضافات/مطورين جيبثون.py
+++ مستخدم الروبوت/الإضافات/مطورين جيبثون.py
@@ -2,4 +2,4 @@ from userbot import jmthon
 from . import *
 @jmthon_on(admin_cmd(pattren="سيد "))
 async def_(dar and darkn):
-await  dar and darkn.edit("مطورين سورس جيبثون")
+await  dar and darkn.edit("مطور سورس جيبثون")
