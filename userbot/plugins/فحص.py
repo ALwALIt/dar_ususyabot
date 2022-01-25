cec915c7f1d7a3a938abe31dd03128a79bb1cc8a
USERBOTJEPTHON <93378583+USERBOTJEPTHON@users.noreply.github.com>
2022-01-25 01:40:34 -0800

    Update فحص.py

--- userbot/plugins/فحص.py
+++ userbot/plugins/فحص.py
@@ -40,7 +40,7 @@ async def amireallyalive(event):
     _, check_sgnirts = check_data_base_heal_th()
     EMOJI = gvarstatus("ALIVE_EMOJI") or "  - "
     ALIVE_TEXT = gvarstatus("ALIVE_TEXT") or "** بـوت جيـبثون يعـمل بنـجـاح **"
-    RR7_IMG = gvarstatus("ALIVE_PIC") or Config.A_PIC
+    RR7_IMG = gvarstatus("ALIVE_PIC") or Config.A_PIC or "https://telegra.ph/file/ffcd337e78cb11cf9efae.mp4"
     jmthon_caption = gvarstatus("ALIVE_TEMPLATE") or temp
     caption = jmthon_caption.format(
         ALIVE_TEXT=ALIVE_TEXT,
