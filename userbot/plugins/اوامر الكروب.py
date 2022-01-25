0a150704733cf121d97308e37a668ea5bc24d12e
USERBOTJEPTHON <93378583+USERBOTJEPTHON@users.noreply.github.com>
2022-01-25 06:29:19 -0800

    Update اوامر الكروب.py

--- userbot/plugins/اوامر الكروب.py
+++ userbot/plugins/اوامر الكروب.py
@@ -24,6 +24,7 @@ from ..core.logger import logging
 from ..core.managers import edit_delete, edit_or_reply
 from ..helpers import readable_time
 from . import BOTLOG, BOTLOG_CHATID
+from ..utils.tools import create_supergroup
 
 LOGS = logging.getLogger(__name__)
 plugin_category = "admin"
@@ -48,6 +49,84 @@ async def ban_user(chat_id, i, rights):
     except Exception as exc:
         return False, str(exc)
 
+@jmthon.ar_cmd(
+    pattern="صنع (مجموعه خارقه|مجموعه عاديه|قناه) ([\s\S]*)",
+    command=("صنع", plugin_category),
+    info={
+        "header": "⌔︙لإنشاء مجموعة خاصة/قناة مع جيبثون العرب  ☸️",
+        "description": "⌔︙إستخدام سمد لإنشاء مجموعة خارقة، مجموعة عادية أو قناة  ⚜️",
+        "flags": {
+            "مجموعه خارقه": "⌔︙لإنشاء مجموعة خارقة خاصة",
+            "مجموعه عاديه": "⌔︙لإنشاء مجموعة أساسية خاصة.",
+            "قناه": "⌔︙لإنشاء قناة خاصة ",
+        },
+        "usage": "{tr}صنع (مجموعه خارقه|مجموعه عاديه|قناه) <اسم المجموعه او القناه>",
+        "examples": "{tr}صنع مجموعه خارقه + اسم الكروب",
+    },
+)
+async def _(event):
+    "⌔︙لإنشاء مجموعة خاصة/قناة مع مستخدم البوت  ☸️"
+    type_of_group = event.pattern_match.group(1)
+    group_name = event.pattern_match.group(2)
+    if type_of_group == "قناه":
+        descript = "⌔︙ هذه قناة إختبار أُنشئت بإستعمال جيبثون العرب"
+    else:
+        descript = "⌔︙ هذه المجموعه إختبار أُنشئت بإستعمال جيبثون العرب"
+    if type_of_group == "مجموعه خارقه":
+        try:
+            result = await event.client(
+                functions.messages.CreateChatRequest(
+                    users=[Config.TG_BOT_USERNAME],
+                   
+                    title=group_name,
+                )
+            )
+            created_chat_id = result.chats[0].id
+            result = await event.client(
+                functions.messages.ExportChatInviteRequest(
+                    peer=created_chat_id,
+                )
+            )
+            await edit_or_reply(
+                event, f"⌔︙ اسم المجموعه `{group_name}` ** تم الإنشاء بنجاح  ✅  دخول ** {result.link}"
+            )
+        except Exception as e:
+            await edit_delete(event, f"**⌔︙ حدث خطأ ما  🆘:**\n{str(e)}")
+    elif type_of_group == "قناه":
+        try:
+            r = await event.client(
+                functions.channels.CreateChannelRequest(
+                    title=group_name,
+                    about=descript,
+                    megagroup=False,
+                )
+            )
+            created_chat_id = r.chats[0].id
+            result = await event.client(
+                functions.messages.ExportChatInviteRequest(
+                    peer=created_chat_id,
+                )
+            )
+            await edit_or_reply(
+                event,
+                f"⌔︙ اسم القناه `{group_name}` ** تم الإنشاء بنجاح  ✅  دخول ** {result.link}",
+            )
+        except Exception as e:
+            await edit_delete(event, f"**⌔︙ حدث خطأ ما  🆘 :**\n{str(e)}")
+    elif type_of_group == "مجموعه خارقه":
+        answer = await create_supergroup(
+            group_name, event.client, Config.TG_BOT_USERNAME, descript
+        )
+        if answer[0] != "error":
+            await edit_or_reply(
+                event,
+                f"⌔︙ ميجا جروب `{group_name}` ** تم الإنشاء بنجاح  ✅  دخول ** {answer[0].link}",
+            )
+        else:
+            await edit_delete(event, f"**⌔︙ حدث خطأ ما  🆘 :**\n{str(answer[1])}")
+    else:
+        await edit_delete(event, "**⌔︙الاوامر` **صنع مجموعه لمعرفة كيفية استخدامي.`")
+    
 @jmthon.ar_cmd(
     pattern="اطردني$",
     command=("اطردني", plugin_category),
