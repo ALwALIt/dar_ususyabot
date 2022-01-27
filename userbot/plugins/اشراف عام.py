8b84694c79f0a54d06c1f1cfa6b66ca55dc6f3d0
USERBOTJEPTHON <93378583+USERBOTJEPTHON@users.noreply.github.com>
2022-01-27 03:22:23 -0800

    Update اشراف عام.py

--- userbot/plugins/اشراف عام.py
+++ userbot/plugins/اشراف عام.py
@@ -14,7 +14,7 @@ from telethon.tl.types import (ChannelParticipantsAdmins,
                                        MessageMediaPhoto)
 from userbot.utils import admin_cmd
 from ..Config import Config
-from userbot import jmthon
+from userbot import CMD_HELP, jmthon
 up_admin = Config.UP_ET or "ارفع"
 down_admin = Config.DOWN_ET or "تزل"
 async def get_full_user(event):  
