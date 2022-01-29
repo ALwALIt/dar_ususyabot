e2bf2d565f2a8d3124e368c98baf4e9fec0c68d3
USERBOTJEPTHON <93378583+USERBOTJEPTHON@users.noreply.github.com>
2022-01-29 22:23:31 +0300

    Update and rename userbot/plugins/التنصيب.py to مستخدم الروبوت/الإضافات/التنصيب.py

--- userbot/plugins/التنصيب.py
+++
@@ -1,9 +0,0 @@
-from telethon import events
-import random, re
-from userbot.utils import admin_cmd
-import asyncio 
-
-@borg.on(admin_cmd("تنصيب السورس"))
-async def _(event):
-     if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
-        await event.edit("⌁︙**اهـلا بك فـي تنصيب السورس** \n⌁︙رابط التنصيب  -[اضغط هنا](https://github.com/USERBOTJEPTHON/JEP-THON/)\n⌁︙قناة السورس - @JepThon")
