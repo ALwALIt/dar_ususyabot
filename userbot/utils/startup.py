6434d959110081ed02141289c8d21c18d4374617
جيبثون العربي <96865194+lMl10l@users.noreply.github.com>
2022-03-18 00:37:35 +0300

    Update startup.py

--- userbot/utils/startup.py
+++ userbot/utils/startup.py
@@ -276,7 +276,7 @@ async def verifyLoggerGroup():
             )
     else:
         descript = "⌯︙ وظيفه الكروب يحفظ رسائل الخاص اذا ما تريد الامر احذف الكروب نهائي \n  - @Jepthon"
-        photobt = await jmthon.upload_file(file="Jmthon/razan/resources/start/Jepthon.jpg")
+        photobt = await jmthon.upload_file(file="Jmthon/razan/resources/start/Jepthon2.jpg")
         _, groupid = await create_supergroup(
             "مجموعة التخزين", jmthon, Config.TG_BOT_USERNAME, descript, photobt
         )
