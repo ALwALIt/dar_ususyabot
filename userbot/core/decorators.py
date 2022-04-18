06f9f560c9acab910f952258529e67cf3fae8d14
ALwALIt <103385382+ALwALIt@users.noreply.github.com>
2022-04-18 17:54:24 +0300

    Update decorators.py

--- userbot/core/decorators.py
+++ userbot/core/decorators.py
@@ -22,7 +22,7 @@ def check_owner(func):
         else:
             HELP_TEXT = (
                 gvarstatus("HELP_TEXT")
-                or "هذا الخيار ليس لك يجب عليك تنصيب سورس جيبثون بنفسك  @Jepthon"
+                or "هذا الخيار ليس لك يجب عليك تنصيب سورس ماتركس بنفسك  @VV399"
             )
             await c_q.answer(
                 HELP_TEXT,
